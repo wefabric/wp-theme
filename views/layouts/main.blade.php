e64dbc9408a2378d653cae308922f9f1d2e2db78
Esther de Vries <estherwefabric@MBP-van-Esther.localdomain>
2022-02-07 12:26:51 +0100

    Removed accidental endif

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -19,7 +19,6 @@
 @if(isset($options['body_codes']) && $options['body_codes'])
     {!! $options['body_codes'] !!}
 @endif
-@endif
 @if(isset($options['out_of_office']['active']) && $options['out_of_office']['active'])
     @if((isset($options['out_of_office']['start_display_date']) && $options['out_of_office']['start_display_date'] <= date('Ymd')) || $options['out_of_office']['start_display_date'])
         @if((isset($options['out_of_office']['end_display_date']) && $options['out_of_office']['end_display_date'] >= date('Ymd')) || $options['out_of_office']['end_display_date'])
