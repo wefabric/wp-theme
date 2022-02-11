0d019c9e56150651208b4711a0c32a5a3596683b
Leo Flapper <dominicus1991@gmail.com>
2022-02-11 11:37:54 +0100

    Fixed default padding for mobile

--- views/page-builder/blocks/textblock/index.blade.php
+++ views/page-builder/blocks/textblock/index.blade.php
@@ -1,3 +1,3 @@
-<div class="container px-4 lg:px-0 mx-auto xl:w-1/2">
+<div class="container px-8 lg:px-0 mx-auto xl:w-1/2">
     @include('components.content', ['content' => apply_filters('the_content', $block->get('text'))])
 </div>
