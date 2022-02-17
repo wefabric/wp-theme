bc62f82eec7e355027f2b5394cd4cbfd09ef7e95
Esther de Vries <estherwefabric@MBP-van-Esther.localdomain>
2022-02-17 08:25:59 +0100

    Added default 404 page styling

--- views/errors/404.blade.php
+++ views/errors/404.blade.php
@@ -1,29 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="error-404 not-found">
-        <header class="page-header">
-            <h1 class="page-title">{!! esc_html__('Oops! That page can&rsquo;t be found.', THEME_TD) !!}</h1>
-        </header><!-- .page-header -->
+    <header class="page-header py-24 bg-primary mb-9">
+        <h1 class="page-title px-8 text-center text-white">{!! esc_html__('Oeps! Deze pagina werd niet gevonden.', THEME_TD) !!}</h1>
+    </header><!-- .page-header -->
+    <section class="error-404 not-found container mx-auto py-9 px-8">
         <div class="page-content">
-            <p>{{ esc_html__('It looks like nothing was found at this location. Maybe try one of the links below or a search?', THEME_TD) }}</p>
-            {!! get_search_form(false) !!}
-            @php(the_widget('WP_Widget_Recent_Posts'))
-            <div class="widget widget_categories">
-                <h2 class="widget-title">{{ esc_html__('Most Used Categories', THEME_TD) }}</h2>
-                <ul>
-                    {!! wp_list_categories([
-                        'orderby' => 'count',
-                        'order' => 'DESC',
-                        'show_count' => 1,
-                        'title_li' => '',
-                        'number' => 10,
-                        'echo' => false
-                    ]) !!}
-                </ul>
-            </div><!-- .widget -->
-            @php(the_widget('WP_Widget_Archives', 'dropdown=1', 'after_title=</h2>'.archive_content_message()))
-            @php(the_widget('WP_Widget_Tag_Cloud'))
+            <p>{{ esc_html__('Het lijkt erop dat deze pagina niet bestaat. Controleer of je de pagina correct hebt geschreven of gebruik het menu om te vinden wat je zoekt.', THEME_TD) }}</p>
         </div>
     </section>
 @endsection
