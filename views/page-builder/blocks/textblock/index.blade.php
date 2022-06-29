73bc15dea6a06d19f82690a75463674ced06ac89
Kevin Nauta <kevin@wefabric.nl>
2022-06-29 15:41:06 +0200

    Cleanup textblock

--- views/page-builder/blocks/textblock/index.blade.php
+++ views/page-builder/blocks/textblock/index.blade.php
@@ -1,3 +1,3 @@
-<div class="container px-8 lg:px-0 mx-auto xl:w-1/2">
+<div class="mx-auto lg:w-3/5">
     @include('components.content', ['content' => apply_filters('the_content', $block->get('text'))])
 </div>
