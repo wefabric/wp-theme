55b809fd1a08cf2aaf7757cb62404271782e87fb
Kevin Nauta <kevin@wefabric.nl>
2022-06-29 15:40:35 +0200

    Add debug-information in pagebuilder render

--- views/page-builder/render.blade.php
+++ views/page-builder/render.blade.php
@@ -5,7 +5,8 @@
         @endphp
 
         <div class="px-8 lg:px-0">
-            <section class="{{ $blockName }} py-12 container mx-auto">
+            <p>Block: {{ $blockName }}</p> {{-- <<    DEBUG REMOVE      v         v     --}}
+            <section class="{{ $blockName }} py-12 container mx-auto  border-2 border-black ">
                 @include('page-builder.blocks.'. $blockName .'.index', ['block' => $block])
             </section>
         </div>
