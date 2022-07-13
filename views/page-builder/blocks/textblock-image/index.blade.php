ed8cd0fd5f55d79275b443c221bbed00a738a7af
Kevin Nauta <kevin@wefabric.nl>
2022-07-13 16:58:16 +0200

    Update textblock-image block

--- views/page-builder/blocks/textblock-image/index.blade.php
+++ views/page-builder/blocks/textblock-image/index.blade.php
@@ -1,5 +1,6 @@
-<div class="mx-auto w-full flex lg:{{ $block->get('width') }}">
-    <div class="lg:w-4/6 @if($block->get('image_position') === 'left') order-1 lg:order-2 pl-8 lg:pl-16 @else pr-8 lg:pr-16 @endif @switch($block->get('image_vertical_position')) {{-- For aligning the text,when shorter than the image. --}}
+<div class="mx-auto w-full lg:flex lg:{{ $block->get('width') }}">
+
+    <div class="w-full lg:w-4/6 @if($block->get('image_position') === 'left') order-1 lg:order-2 lg:pl-16 @else lg:pr-16 @endif @switch($block->get('image_vertical_position')) {{-- For aligning the text,when shorter than the image. --}}
             @case('middle') lg:my-auto @break
             @case('bottom') lg:mt-auto lg:mb-0 @break;
         @endswitch ">
@@ -8,14 +9,14 @@
             'class' => 'text-justify'
         ])
     </div>
-    <div class="lg:w-2/6 @if($block->get('image_position') === 'left') order-2 lg:order-1 @endif @switch($block->get('image_vertical_position'))
+
+    <div class="w-full lg:w-2/6 pt-8 lg:pt-0 @if($block->get('image_position') === 'left') order-2 lg:order-1 @endif @switch($block->get('image_vertical_position'))
             @case('middle') lg:my-auto @break
             @case('bottom') lg:mt-auto lg:mb-0 @break;
         @endswitch ">
         @if($block->get('image'))
             @include('components.image', [
                 'image_id' => $block->get('image'),
-                'alt' => '',
                 'class' => 'w-full h-full object-cover'
             ])
         @endif
