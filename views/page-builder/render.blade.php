242f1d9e808e877ed4578e8f189bdf95016fa3ee
Kevin Nauta <kevin@wefabric.nl>
2022-07-28 17:32:57 +0200

    add background/text color to all pagebuilder blocks

--- views/page-builder/render.blade.php
+++ views/page-builder/render.blade.php
@@ -4,7 +4,7 @@
             $blockName = strConvert($block->get('acf_fc_layout'))->toKebab();
         @endphp
 
-        <div class="bg-{{ $block->get('bg_color') }} text-{{ $block->get('text_color') }}">
+        <div class="bg-{{ $block->get('bg_color') ?? 'white' }} text-{{ $block->get('text_color') ?? 'black' }}">
             <div class="px-8 lg:px-0 pb-8">
                 <p>Block: {{ $blockName }}</p> {{-- <<    DEBUG REMOVE      v         v     --}}
                 <section class="{{ $blockName }} py-12 container mx-auto  border-2 border-pink-500 ">
