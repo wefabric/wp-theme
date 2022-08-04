a45847ef2ef5fd560724c5cdd08fa6735daea73b
Leo Flapper <dominicus1991@gmail.com>
2022-08-04 22:56:20 +0200

    Added contact block fill color and image

--- views/page-builder/blocks/contact-form/index.blade.php
+++ views/page-builder/blocks/contact-form/index.blade.php
@@ -6,21 +6,33 @@
 <div class="container mx-auto w-full lg:{{ $block->get('width') }} @if($block->get('show_address', false)) bg-{{ $block->get('bg_color') }} text-{{ $block->get('text_color') }} @endif pt-4 lg:pt-8">
 
     <div class="mt-6 mt-md-10 container position-relative z-index-2">
-        <div class="@if($block->get('show_address', false)) md:grid md:grid-cols-2 gap-8 @endif" >
-            <div class="bg-{{ str_replace('-color', '', $block->get('background_color', 'bg_color')) }} rounded-lg  text-{{ $block->get('text_color') }}">
+        <div class="relative @if($block->get('fill_color')) bg-{{ $block->get('fill_color') }} p-12 @endif @if($block->get('show_address', false)) md:grid md:grid-cols-2 gap-8 @endif" >
+            <div class="lg:{{ $block->get('width_form') }} bg-{{ str_replace('-color', '', $block->get('background_color', 'bg_color')) }} rounded-lg  text-{{ $block->get('text_color') }}">
                 @if($block->get('show_separate_title'))
                     @include('components.headings.normal', [
                         'type' => 2,
                         'title' => $block->get('title'),
+                        'class' => 'mb-8',
                         'text_color' => $block->get('text_color'),
                         'show_line' => $block->get('show_line')
                     ])
                 @endif
                 {!! $block->get('form') !!}
+
+                @if($block->get('image') && $block->get('image')->get('image'))
+                    <div class="absolute -right-2 md:-right-6 md:-bottom-16">
+                        @include('components.image', [
+                            'image_id' => $block->get('image')->get('image'),
+                            'size' => 'full',
+                            'class' => 'rounded-lg max-w-[150px] lg:max-w-none',
+                        ])
+                    </div>
+
+                @endif
             </div>
 
             @if($block->get('show_address', false))
-                <div class="offset-md-1 col-md-4 text-base">
+                <div class="offset-md-1 mt-12 md:mt-0 col-md-4 text-base">
                     <div class="widget widget-address">
                         <div class=" border-b-[1px] pb-4 border-[#DED8FF]">
                             <h2 class="text-lg  mb-6">{{ 'Contact' }}</h2>
@@ -82,23 +94,3 @@
     </div>
 
 </div>
-
-{{--<div>--}}
-{{--    <label>--}}
-{{--        Naam--}}
-{{--        [text* your-name class:full-width class:rounded placeholder "Naam"]--}}
-{{--    </label>--}}
-{{--    <label>--}}
-{{--        E-mailadres--}}
-{{--        [email* your-email class:full-width placeholder "E-mailadres"]--}}
-{{--    </label>--}}
-{{--    <label>--}}
-{{--        Telefoonnummer--}}
-{{--        [text your-phone class:full-width placeholder "Telefoonnummer"]--}}
-{{--    </label>--}}
-{{--    <label>--}}
-{{--        Bericht--}}
-{{--        [textarea* your-message class:full-width placeholder "Bericht"]--}}
-{{--    </label>--}}
-{{--    [submit class:btn class:btn-primary class:pull-right "Verstuur"]--}}
-{{--</div>--}}
