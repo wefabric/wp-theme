5d3cc628497e5f707f687a2af06a6da63eb2789b
Kevin Nauta <kevin@wefabric.nl>
2022-08-04 10:36:54 +0200

    Fix Alpine JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
+import Alpine from 'alpinejs'
+
 // Import dependencies
 window.$ = window.jQuery = require('jquery');
-import 'slick-carousel';
-
-// Import default theme JS files
-import './sliders';
 
 // Import custom JS files
 import './outOfOffice';
 import './forms';
+
+window.Alpine = Alpine
+Alpine.start()
