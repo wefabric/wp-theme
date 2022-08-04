3f2ce12e8882c4be9e09a2ba36b93646dc4e60cf
Kevin Nauta <kevin@wefabric.nl>
2022-08-04 15:47:53 +0200

    Fix textblock-image

--- views/page-builder/blocks/textblock-image/index.blade.php
+++ views/page-builder/blocks/textblock-image/index.blade.php
@@ -1,29 +1,32 @@
-<div class="container mx-auto w-full lg:flex lg:{{ $block->get('width') }}">
+<div class="container mx-auto w-full lg:{{ $block->get('width') }} lg:h-[{{ $block->get('image')->get('height_desktop') }}]">
 
-    <div class="relative z-10 w-full lg:{{ $block->get('image_width') }}  lg:h-[{{ $block->get('image')->get('height_desktop') }}] @if($block->get('image')->get('position') === 'left') order-1 lg:order-2 lg:pl-16 @else lg:pr-16 @endif"
+    <div class="absolute z-10 w-full lg:{{ $block->get('image_width') }}  lg:h-[{{ $block->get('image')->get('height_desktop') }}] bg-no-repeat  @if($block->get('image')->get('position') === 'left') order-1 lg:order-2 @endif"
         style="background-image: url('{{ wp_get_attachment_image_url($block->get('image')->get('image'), 'textblock-image') }}')">
     </div>
 
-    <div class="relative z-50 h-full w-full lg:{{ $block->get('text_width') }} lg:my-auto lg:px-16 lg:py-20 @if($block->get('image')->get('position') === 'left') order-2 lg:order-1 @endif">
-        @if($block->get('show_separate_title'))
-            @include('components.headings.normal', [
-	            'type' => 2,
-	            'title' => $block->get('title'),
-            ])
-        @endif
-
-        @include('components.content', [
-            'content' => apply_filters('the_content', $block->get('text')),
-            'class' => 'text-justify'
-        ])
-
-        @if($block->get('show_button'))
-            <div class="flex justify-{{ $block->get('button')->get('position') }}">
-                @include('components.buttons.default', [
-                    'button' => $block->get('button'),
-                    'class' => 'disable-chevron font-bold'
+    <div class="flex justify-end lg:pt-32">
+        <div class="relative z-50 h-full w-full lg:{{ $block->get('text_width') }} lg:px-16 lg:py-20 bg-white ">
+            @if($block->get('show_separate_title'))
+                @include('components.headings.normal', [
+                    'type' => 2,
+                    'title' => $block->get('title'),
+                    'class' => 'pb-4 lg:pb-8'
                 ])
-            </div>
-        @endif
+            @endif
+
+            @include('components.content', [
+                'content' => apply_filters('the_content', $block->get('text')),
+                'class' => 'text-justify'
+            ])
+
+            @if($block->get('show_button'))
+                <div class="flex justify-{{ $block->get('button')->get('position') }}">
+                    @include('components.buttons.default', [
+                        'button' => $block->get('button'),
+                        'class' => 'disable-chevron font-bold'
+                    ])
+                </div>
+            @endif
+        </div>
     </div>
 </div>
