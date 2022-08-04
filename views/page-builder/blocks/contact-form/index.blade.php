6b83ab7d46c19659c5640c6aad8246e867d5e115
Kevin Nauta <kevin@wefabric.nl>
2022-08-04 12:21:27 +0200

    Remove pagebuilder block newsletter and rename to contactform

--- views/page-builder/blocks/contact-form/index.blade.php
+++ views/page-builder/blocks/contact-form/index.blade.php
@@ -1,14 +1,31 @@
-<div class="container mx-auto">
-    @include('components.headings.normal', [
-	    'type' => 2,
-	    'heading' => $block->get('title'),
-    ])
+<div class="container mx-auto w-full lg:{{ $block->get('width') }} bg-{{ $block->get('bg_color') }} text-{{ $block->get('text_color') }}  pt-4 lg:pt-8">
 
-    @include('components.content', [
-        'content' => $block->get('content'),
-    ])
+    <div class="bg-{{ $block->get('fill_color') }} lg:p-16 lg:pr-64 relative ">
 
-    <div class="mt-3">
-        {!! $block->get('form') !!}
+        @if($block->get('title'))
+            @include('components.headings.normal', [
+                'type' => 3,
+                'title' => $block->get('title'),
+                'text_color' => $block->get('text_color'),
+            ])
+        @endif
+
+        <div class="mt-3">
+            {!! $block->get('form') !!}
+        </div>
+
+        <div class="">
+            By sending this form, you’ve red and agreed with our privacy policy.
+        </div>
+
+        <div class="absolute ">
+            @include('components.image', [
+	            'image_id' => $block->get('image')->get('image'),
+	            'size' => '', // 250x200
+
+            ])
+        </div>
+
     </div>
+
 </div>
