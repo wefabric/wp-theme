dd39b0eda0a7f2c435e7819783975527222cd46a
Kevin Nauta <kevin@wefabric.nl>
2022-09-20 17:28:52 +0200

    update footer with two establishments

--- views/components/establishments/directions.blade.php
+++ views/components/establishments/directions.blade.php
@@ -1,8 +1,14 @@
-@include('components.link.opening', [
-       'href' => $establishment->getAddress()->getGoogleMapsUrl(),
-       'alt' => 'Adres op Google Maps',
-       'class' => 'flex mb-2 no-underline'
-   ])
-    <i class="fa-solid fa-location-dot mr-4 ml-1 text-md pt-1"></i>
-    <span class="inline-block pt-1 cursor-pointer">Routebeschrijving</span>
-@include('components.link.closing')
+@php
+	$address = $establishment->getAddress();
+@endphp
+
+@if($establishment->getAcfFields()->get('allow_route') && !empty($address->street) && !in_array(strtolower($address->street), ['postbus', 'antwoordnummer']))
+	@include('components.link.opening', [
+		   'href' => $address->getGoogleMapsUrl(),
+		   'alt' => 'Adres op Google Maps',
+		   'class' => 'flex'
+	   ])
+	<i class="fa-solid fa-location-dot mr-4 ml-1 text-md pt-1"></i>
+	<span class="inline-block pt-1">Routebeschrijving</span>
+	@include('components.link.closing')
+@endif
--- views/components/footer/contact.blade.php
+++ views/components/footer/contact.blade.php
@@ -1,37 +1,63 @@
-@php $establishment = \Wefabric\WPEstablishments\Establishment::primary(); @endphp
+@php
+	$option = get_fields('option');
+	
+	if(!empty($option)) {
+		if(array_key_exists('footer_establishments', $option)) {
+			$footer_establishments = $option['footer_establishments'];
+		}
+	}
+
+	if(empty($footer_establishments) || empty($footer_establishments[0])) {
+		$footer_establishments = [];
+		$footer_establishments[] = \Wefabric\WPEstablishments\Establishment::primary();
+	}
+@endphp
+
 <div class="text-base mb-6 leading-7">
-    <div class=" pt-2 mb-4">
-        @if($establishment)
-            <p>{{ $establishment->name }}</p>
-            <p>{{ $establishment->getAddress()->street }} {{ $establishment->getAddress()->full_housenumber }}</p>
-            <p>{{ $establishment->getAddress()->postcode }} {{ $establishment->getAddress()->city }}</p>
-        @endif
-    </div>
-
-    @if($establishment && $phone = $establishment->getContactPhone())
-        @include('components.link.opening', [
-            'href' => $phone->uri(), //comes with a 'tel:' already
-            'alt' => 'Telefoonnummer',
-            'class' => 'flex mb-2 no-underline',
-        ])
-            <i class="fa-solid fa-phone mr-4 text-md pt-1"></i>
-            <span class="inline-block pt-1 cursor-pointer">{{ $phone }}</span>
-        @include('components.link.closing')
-    @endif
-
-    @if($establishment && $email = $establishment->getContactEmailAddress())
-        @include('components.link.opening', [
-            'href' => 'mailto:'. $email,
-            'alt' => 'E-mailadres',
-            'class' => 'flex mb-2 no-underline',
-        ])
-            <i class="fa-solid fa-envelope mr-4 text-md pt-1"></i>
-            <span class="inline-block pt-1 cursor-pointer">{{ $email }}</span>
-        @include('components.link.closing')
-    @endif
-
-    @if($establishment)
-        @include('components.establishments.directions')
-    @endif
+	@foreach($footer_establishments as $key => $establishment_config)
+		@php
+			/* @var WP_Post $establishment_config */
+			$establishment = new \Wefabric\WPEstablishments\Establishment($establishment_config['establishment']);
+		@endphp
+		
+		<div class="mb-10 footer-address">
+			@if($establishment)
+				@if($establishment_config['show_title'])
+					<div class="text-lg font-bold pb-4">
+						{{ $establishment->name }}
+					</div>
+				@endif
+				
+				<p class="leading-8">
+					{{ $establishment->getAddress()->street }} {{ $establishment->getAddress()->full_housenumber }} <br/>
+					{{ $establishment->getAddress()->postcode }} {{ $establishment->getAddress()->city }}
+				</p>
+				
+				@if($phone = $establishment->getContactPhone())
+					@include('components.link.opening', [
+						'href' => $phone->uri(), //comes with a 'tel:' already
+						'alt' => 'Telefoonnummer',
+						'class' => 'flex'
+					])
+					<i class="fa-solid fa-phone mr-4 text-md pt-1"></i>
+					<span class="inline-block pt-1 cursor-pointer hover:underline">{{ $phone }}</span>
+					@include('components.link.closing')
+				@endif
+				
+				@if($email = $establishment->getContactEmailAddress())
+					@include('components.link.opening', [
+						'href' => 'mailto:'. $email,
+						'alt' => 'E-mailadres',
+						'class' => 'flex'
+					])
+					<i class="fa-solid fa-envelope mr-4 text-md pt-1"></i>
+					<span class="inline-block pt-1 cursor-pointer hover:underline">{{ $email }}</span>
+					@include('components.link.closing')
+				@endif
+				
+				@include('components.establishments.directions')
+			@endif
+		</div>
+	@endforeach
 
 </div>
