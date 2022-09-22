10786eef642c6b9057494bc9d205bcff84e58d03
Kevin Nauta <kevin@wefabric.nl>
2022-09-22 10:22:39 +0200

    Fix breadcrumbs, add to default pages

--- views/components/breadcrumbs/index.blade.php
+++ views/components/breadcrumbs/index.blade.php
@@ -1,10 +1,10 @@
-<div class="flex mx-auto mt-12 {{ $classes ?? 'w-4/5 lg:w-3/5' }} {{ $text_color ?? 'text-purple' }} ">
+<div class="flex mx-auto mt-12 text-xs {{ $classes ?? 'w-4/5 lg:w-3/5' }} {{ $text_color ?? 'text-purple' }} ">
 	@include('components.link.opening', [
 		'href' => get_home_url(),
 		'alt' => 'Homepagina',
 		'class' => 'self-center mr-1',
 	])
-        <div class="self-center {{ $icon ?? 'breadcrumb-home' }}"></div>
+		<i class="fa-solid fa-house mr-4"></i>
 	@include('components.link.closing')
 	
     <div>
--- views/pages/default.blade.php
+++ views/pages/default.blade.php
@@ -5,6 +5,13 @@
         <div class="header">
             {!! themeHeader()->render() !!}
         </div>
+
+		@if(!is_front_page())
+			<div class="container mx-auto px-8 lg:px-0">
+				@include('components.breadcrumbs.index', ['classes' => ''])
+			</div>
+		@endif
+	
         <div class="page-builder">
             {!! pageBuilder()->render() !!}
         </div>
