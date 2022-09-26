ff824ff3f2750b184f340485c4fdf5a87a1ab6b0
Kevin Nauta <kevin@wefabric.nl>
2022-09-26 16:59:38 +0200

    Fix footer text size

--- views/components/footer/accordion-menu.blade.php
+++ views/components/footer/accordion-menu.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-3">
-    <div class="text-sm leading-9">
+    <div class="leading-relaxed">
         <div class="@if($setAccordion === true) accordion-drawer @endif">
             @if($setAccordion === true) <input class="accordion-drawer__trigger mb-4" id="accordion-drawer-{{ $accordionId }}" type="checkbox" /> @endif
             <label class="accordion-drawer__title relative block text-lg font-bold py-4" for="accordion-drawer-{{ $accordionId }}">
