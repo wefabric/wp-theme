bc716806f89606b0ea52ab592d9d832cf1c898a0
Kevin Nauta <kevin@wefabric.nl>
2022-10-27 12:28:21 +0200

    Add FAQ blocks to product detail page

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -29,28 +29,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $product_tabs ) ) : ?>
-
-	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs" role="tablist">
+	
+	<div class="container mx-auto w-full"  style="clear:both">
+		
+		<div class="faq-drawer lg:px-0">
 			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>">
-						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
-					</a>
-				</li>
+				<div class="faq-drawer__block <?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+					<input class="faq-drawer__trigger" id="faq-drawer-<?php echo esc_attr( $key ); ?>" type="checkbox" />
+					<label class="faq-drawer__title " for="faq-drawer-<?php echo esc_attr( $key ); ?>"><?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?></label>
+					
+					<div class="faq-drawer__content-wrapper">
+						<div class="faq-drawer__content rounded-lg bg-gray-100 text-black z-20 p-6">
+							<?php
+							if ( isset( $product_tab['callback'] ) ) {
+								call_user_func( $product_tab['callback'], $key, $product_tab );
+							}
+							?>
+						</div>
+					</div>
+				</div>
 			<?php endforeach; ?>
-		</ul>
-		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
-				<?php
-				if ( isset( $product_tab['callback'] ) ) {
-					call_user_func( $product_tab['callback'], $key, $product_tab );
-				}
-				?>
-			</div>
-		<?php endforeach; ?>
-
+		</div>
+		
 		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
+	
 	</div>
+
 
 <?php endif; ?>
