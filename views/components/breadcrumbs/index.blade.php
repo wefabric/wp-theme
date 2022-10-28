83f22501e26620ed092a34f1ddcd110ea91b4bea
Leo Flapper <dominicus1991@gmail.com>
2022-10-28 16:11:50 +0200

    Added rankmath breadcrumbs if available

--- views/components/breadcrumbs/index.blade.php
+++ views/components/breadcrumbs/index.blade.php
@@ -2,18 +2,9 @@
 	<div class="container mx-auto px-8 lg:px-24">
 		<div class="flex mx-auto text-base {{ $classes ?? 'w-4/5 lg:w-3/5' }}">
 
-{{--
-			@include('components.link.opening', [
-				'href' => get_home_url(),
-				'alt' => 'Homepagina',
-				'class' => 'self-center mr-1',
-			])
-				<i class="fa-solid fa-house mr-4"></i>
-			@include('components.link.closing')
---}}
-			
 			<div>
-				{!! yoast_breadcrumb() !!}
+				@php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); @endphp
+				@php if (function_exists('yoast_breadcrumb')) echo yoast_breadcrumb(); @endphp
 			</div>
 		</div>
 	</div>
