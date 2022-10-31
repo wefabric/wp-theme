b7461417562ab7019fc3209118b177463545e00f
Leo Flapper <dominicus1991@gmail.com>
2022-10-31 16:01:21 +0100

    Added fallback header for posts

--- views/pages/default.blade.php
+++ views/pages/default.blade.php
@@ -2,14 +2,21 @@
 
 @section('content')
     @loop
-        <div class="header">
-            {!! themeHeader()->render() !!}
-        </div>
+
+        @if(get_post_type() === 'post' && themeHeader()->getHeaderValues(get_the_ID())->count() === 0)
+            <div class="header">
+                {!! (new \App\Helpers\Post())->renderDefaultHeader(get_post()) !!}
+            </div>
+        @else
+            <div class="header">
+                {!! themeHeader()->render() !!}
+            </div>
+        @endif
 
 		@if(!is_front_page())
 			@include('components.breadcrumbs.index', ['classes' => ''])
 		@endif
-	
+
         <div class="page-builder">
             {!! pageBuilder()->render() !!}
         </div>
