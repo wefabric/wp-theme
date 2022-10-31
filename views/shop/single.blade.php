8bf97654177dc227264fc352999d847a05c97c87
Kevin Nauta <kevin@wefabric.nl>
2022-10-31 16:45:33 +0100

    Fix description tabs on product detail; align add-to-cart button right

--- views/shop/single.blade.php
+++ views/shop/single.blade.php
@@ -4,17 +4,22 @@
 	<div class="container mx-auto">
 		@php(do_action('woocommerce_before_main_content'))
 		
-		<div class="">
+		<div class="woocommerce-product-container">
 			@loop
 			@php(do_action('woocommerce_before_single_product'))
-			<div id="product-{{ Loop::id() }}" {{ wc_product_class('lg:mt-24') }}>
-				@php(do_action('woocommerce_before_single_product_summary'))
-				
+			<div id="product-{{ Loop::id() }}" {{ wc_product_class('lg:mt-24 lg:grid-cols-2 lg:gap-8') }}>
+				<div>
+					@php(do_action('woocommerce_before_single_product_summary'))
+				</div>
+	
 				<div class="summary entry-summary">
 					@php(do_action('woocommerce_single_product_summary'))
 				</div>
 				
-				@php(do_action('woocommerce_after_single_product_summary'))
+				<div class="col-span-2">
+					@php(do_action('woocommerce_after_single_product_summary'))
+				</div>
+				
 			</div>
 			@php(do_action('woocommerce_after_single_product'))
 			@endloop
