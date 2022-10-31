8ff88a9667e3e40a419bc482e5df654bbc0be983
Kevin Nauta <kevin@wefabric.nl>
2022-10-31 14:56:14 +0100

    Fix FAQ styling

--- views/page-builder/blocks/faq/index.blade.php
+++ views/page-builder/blocks/faq/index.blade.php
@@ -18,10 +18,10 @@
             @foreach($faqs as $key => $faq)
 				<div class="faq-drawer__block">
 					<input class="faq-drawer__trigger" id="faq-drawer-{{$key}}" type="checkbox" />
-					<label class="faq-drawer__title " for="faq-drawer-{{$key}}">{{ $faq['question_and_answer']['question'] }}</label>
+					<label class="faq-drawer__title p-6" for="faq-drawer-{{$key}}">{{ $faq['question_and_answer']['question'] }}</label>
 
 					<div class="faq-drawer__content-wrapper">
-						<div class="faq-drawer__content rounded-lg bg-gray-100 text-black z-20 p-6">
+						<div class="faq-drawer__content z-20 p-6">
 							<p class="">{{ $faq['question_and_answer']['answer'] }}</p>
 						</div>
 					</div>
