06f3dbc06bdc4fa113afd12fdb81b9046018f9d7
Leo Flapper <dominicus1991@gmail.com>
2022-11-23 17:54:59 +0100

    Fix for FAQ drawer

--- views/page-builder/blocks/faq/index.blade.php
+++ views/page-builder/blocks/faq/index.blade.php
@@ -15,10 +15,11 @@
 		@endif
 		
         <div class="faq-drawer lg:px-0">
+			@php $id = \Illuminate\Support\Str::random(6); @endphp
             @foreach($faqs as $key => $faq)
 				<div class="faq-drawer__block">
-					<input class="faq-drawer__trigger" id="faq-drawer-{{$key}}" type="checkbox" />
-					<label class="faq-drawer__title p-6" for="faq-drawer-{{$key}}">{{ $faq['question_and_answer']['question'] }}</label>
+					<input class="faq-drawer__trigger" id="faq-drawer-{{$id}}-{{$key}}" type="checkbox" />
+					<label class="faq-drawer__title p-6" for="faq-drawer-{{$id}}-{{$key}}">{{ $faq['question_and_answer']['question'] }}</label>
 
 					<div class="faq-drawer__content-wrapper">
 						<div class="faq-drawer__content z-20 p-6">
