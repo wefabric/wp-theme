d3769ab6922ca4119d700aba3f90633fe18c3922
Leo Flapper <dominicus1991@gmail.com>
2022-12-06 15:32:59 +0100

    Styling fixes

--- views/woocommerce/single-product/additional-summary.blade.php
+++ views/woocommerce/single-product/additional-summary.blade.php
@@ -1,16 +1,19 @@
-<div class="text-base pt-3 @if($product->get_type() !== 'simple') pb-8 @endif" >
-    <span class="font-bold">Artikelnummer: </span> {{ $product->get_sku() }}
+<div class="pb-8 flex flex-col pt-3 gap-1">
+    <div class="text-base " >
+        <span class="font-bold">Artikelnummer: </span> {{ $product->get_sku() }}
+    </div>
+
+    @if($product->get_type() === 'simple')
+        <div class="text-base">
+            <span class="font-bold">Afmetingen: </span> {{ $product->get_length() }} x {{  $product->get_width() }} x {{ $product->get_height() }} mm
+        </div>
+    @endif
+
+    @if($product->get_attribute('eenheid'))
+        <div class="text-base">
+            <span class="font-bold">Afname: </span>
+            {{ \App\Helpers\Product::getQuantityUnit($product) }}
+        </div>
+    @endif
 </div>
 
-@if($product->get_type() === 'simple')
-    <div class="text-base pt-2 ">
-        <span class="font-bold">Afmetingen: </span> {{ $product->get_length() }} x {{  $product->get_width() }} x {{ $product->get_height() }} mm
-    </div>
-@endif
-
-@if($product->get_attribute('eenheid'))
-    <div class="text-base pt-2 pb-8">
-        <span class="font-bold">Afname: </span>
-        {{ \App\Helpers\Product::getQuantityUnit($product) }}
-    </div>
-@endif
