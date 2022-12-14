0629b6ab0f07f52a301372b92881a6228086b986
Leo Flapper <dominicus1991@gmail.com>
2022-12-14 17:23:21 +0100

    Added target blank for technical sheet

--- views/woocommerce/single-product/technical-sheet.blade.php
+++ views/woocommerce/single-product/technical-sheet.blade.php
@@ -1,5 +1,5 @@
 @if($technicalSheet = get_field('technical_sheet', $product->get_id()))
-    <a class="inline-block mb-4" href="{{ wp_get_attachment_url($technicalSheet) }}">
-        <span class="inline-block rounded-full px-4 py-2 text-primary border-primary border mb-4 hover:bg-primary hover:text-white transition-all" target="_blank"> Technisch gegevensblad <i class="fa-regular fa-file-pdf pl-2"></i></span>
+    <a class="inline-block mb-4" href="{{ wp_get_attachment_url($technicalSheet) }}" target="_blank">
+        <span class="inline-block rounded-full px-4 py-2 text-primary border-primary border mb-4 hover:bg-primary hover:text-white transition-all" > Technisch gegevensblad <i class="fa-regular fa-file-pdf pl-2"></i></span>
     </a>
 @endif
