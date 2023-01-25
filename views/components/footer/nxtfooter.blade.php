2ebacfb16f0bce681e07653559633bc3fe709d69
Gerrit Wiersma <info@gwmedia.nl>
2023-01-25 16:34:02 +0100

    Footer aanpassing

--- views/components/footer/nxtfooter.blade.php
+++ views/components/footer/nxtfooter.blade.php
@@ -3,10 +3,8 @@
     <div class="grid gap-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
       <div class="flex lg:justify-start md:justify-center sm:justify-center items-center">
         <ul class="footer-links">
+          <li><a href="" title="">&copy; NXT event <?php echo date('Y'); ?></a></li>
           @php wp_nav_menu( array('menu' => 'Copyright', 'container' => '', 'items_wrap' => '%3$s' )); @endphp
-          <li><a href="" title="">&copy; NXT event 2023</a></li>
-          <li><a href="" title="">Privacybeleid</a></li>
-          <li><a href="" title="">Algemene voorwaarden</a></li>
         </ul>
       </div>
       <div class="flex justify-end lg:justify-end lg:items-center md:justify-center sm:justify-center">
