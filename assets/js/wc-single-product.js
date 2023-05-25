9ebaa55e40be59cfaeb94a7cc16f74b3c1218576
Leo Flapper <dominicus1991@gmail.com>
2023-05-25 14:55:02 +0200

    Deactivated automatic select

--- assets/js/wc-single-product.js
+++ assets/js/wc-single-product.js
@@ -1,34 +1,32 @@
 jQuery( document ).ready(function($) {
     $(document).on( 'change', '.variations select', function( event ) {
-
-        console.log('yes');
-
-        var select = $(this);
-        var variations = $(this).parent().closest('.variations');
-
-        $(variations).find('select').not(select).each(function() {
-
-            var val = $(this).val();
-
-            if($(this).find('option').length > 2) {
-                return false;
-            }
-
-            if ( !val || ( val && !$(this).find('option[value='+val+']:enabled') ) ) {
-
-                $(this).find('option:enabled').each(function() {
-
-                    if ( $(this).attr('value') ) {
-
-                        $(this).prop('selected', 'selected');
-                        return false;
-
-                    }
-
-                });
-
-            }
-
-        });
+        //
+        // var select = $(this);
+        // var variations = $(this).parent().closest('.variations');
+        //
+        // $(variations).find('select').not(select).each(function() {
+        //
+        //     var val = $(this).val();
+        //
+        //     if($(this).find('option').length > 2) {
+        //         return false;
+        //     }
+        //
+        //     if ( !val || ( val && !$(this).find('option[value='+val+']:enabled') ) ) {
+        //
+        //         $(this).find('option:enabled').each(function() {
+        //
+        //             if ( $(this).attr('value') ) {
+        //
+        //                 console.log('selecting: ' + $(this).attr('value'));
+        //                 $(this).prop('selected', 'selected');
+        //                 return false;
+        //
+        //             }
+        //
+        //         });
+        //     }
+        //
+        // });
     });
 });
