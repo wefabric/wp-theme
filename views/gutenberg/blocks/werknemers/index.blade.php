5aab0b60ffc4c1254bcb7b0f9e3ad5325c604ab8
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-07-06 12:19:24 +0200

    Updated werknemers block

--- views/gutenberg/blocks/werknemers/index.blade.php
+++ views/gutenberg/blocks/werknemers/index.blade.php
@@ -33,25 +33,33 @@
     }
 
 
-    $blockWidth = $block['data']['block_width'] ?? 25;
+    $blockWidth = $block['data']['block_width'] ?? 100;
     $blockClass = '';
     if ($blockWidth == 50) {
-        $blockClass = 'container mx-auto w-full lg:w-1/2';
+        $blockClass = 'w-full lg:w-1/2';
+    }
+    elseif ($blockWidth == 66) {
+        $blockClass = 'w-full lg:w-2/3';
     }
     elseif ($blockWidth == 100) {
-        $blockClass = 'container mx-auto w-full';
+        $blockClass = ' w-full';
     }
     elseif ($blockWidth == 'fullscreen') {
-        $blockClass = ' w-full';
+        $blockClass = 'w-full';
+    }
+
+    $fullScreenClass = '';
+    if ($blockWidth !== 'fullscreen') {
+        $fullScreenClass = 'container mx-auto';
     }
 
 @endphp
 
 <section id="werknemers-block" class="bg-{{ $backgroundColor}}">
-    <div class="{{ $blockClass }} px-8 lg:py-20">
-        <div class="container mx-auto">
-            <h2 class="mb-20 {{ $textClass }}">{{ $title }}</h2>
+    <div class="{{ $fullScreenClass }} px-8 py-8 lg:py-20">
+        <div class="{{ $blockClass }} mx-auto">
+            <h2 class="container mx-auto mb-8 lg:mb-20 {{ $textClass }}">{{ $title }}</h2>
+            @include('components.employees.list', ['employees' => $employees])
         </div>
-        @include('components.employees.list', ['employees' => $employees])
     </div>
 </section>
