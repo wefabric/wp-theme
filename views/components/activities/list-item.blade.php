62df102715784a10e125675fadc16aef55149019
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-09-28 16:26:20 +0200

    Updated news and activity block

--- views/components/activities/list-item.blade.php
+++ views/components/activities/list-item.blade.php
@@ -16,7 +16,9 @@
         @if ($activityThumbnailUrl)
             <div class="h-[360px] overflow-hidden w-full relative">
                 <div class="absolute w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out"></div>
-                <div class="absolute z-20 top-[15px] left-[15px] bg-secondary px-4 py-2 rounded-full text-black">Categorie</div>
+                @if (!empty($visibleElements) && in_array('category', $visibleElements))
+                    <div class="absolute z-20 top-[15px] left-[15px] bg-secondary px-4 py-2 rounded-full text-black">Categorie</div>
+                @endif
                 @if ($fields['activity_full'])
                     <div class="absolute z-20 top-[15px] right-[15px] bg-red-500 px-4 py-2 rounded-full text-white">Vol</div>
                 @endif
@@ -27,34 +29,34 @@
 
             <p class="font-bold text-lg">{{ $activityTitle }}</p>
 
-            @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($activitySummary))
-                <p class="mt-3 mb-3">{{ $activitySummary }} </p>
-            @endif
+            <div class="activity-data mt-4">
+                @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($activitySummary))
+                    <p class="mt-3 mb-3">{{ $activitySummary }} </p>
+                @endif
 
-            @if (!empty($visibleElements) && in_array('location', $visibleElements) && !empty($fields['location']))
-                <p class="">
-                    <i class="fas fa-map-marker-alt mr-2"></i>{{ $fields['location'] }}
-                </p>
-            @endif
+                @if (!empty($visibleElements) && in_array('location', $visibleElements) && !empty($fields['location']))
+                    <p class="">
+                        <i class="fas fa-map-marker-alt mr-2"></i>{{ $fields['location'] }}
+                    </p>
+                @endif
 
-            @if (!empty($visibleElements) && in_array('date', $visibleElements) && !empty($fields['date']))
-                <p class="">
-                    <i class="fas fa-calendar-alt mr-2"></i>{{ $fields['date'] }}
-                </p>
-            @endif
+                @if (!empty($visibleElements) && in_array('date', $visibleElements) && !empty($fields['date']))
+                    <p class="">
+                        <i class="fas fa-calendar-alt mr-2"></i>{{ $fields['date'] }}
+                    </p>
+                @endif
 
-
-{{--            @if (!empty($visibleElements) && in_array('button', $visibleElements))--}}
-{{--                <div class="flex items-center flex-wrap">--}}
-
-{{--                    <a class="text-primary inline-flex items-center md:mb-2 lg:mb-0">Lees meer--}}
-{{--                        <svg class="w-4 h-4 ml-2 group-hover:translate-x-1 group-hover:scale-110 transition duration-300 ease-in-out" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">--}}
-{{--                            <path d="M5 12h14"></path>--}}
-{{--                            <path d="M12 5l7 7-7 7"></path>--}}
-{{--                        </svg>--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--            @endif--}}
+                @if (!empty($visibleElements) && in_array('button', $visibleElements))
+                    <div class="mt-4 flex items-center flex-wrap">
+                        <a class="text-primary inline-flex items-center md:mb-2 lg:mb-0">Lees meer
+                            <svg class="w-4 h-4 ml-2 group-hover:translate-x-1 group-hover:scale-110 transition duration-300 ease-in-out" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M5 12h14"></path>
+                                <path d="M12 5l7 7-7 7"></path>
+                            </svg>
+                        </a>
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>
--- views/components/news/list-item.blade.php
+++ views/components/news/list-item.blade.php
@@ -18,7 +18,9 @@
         @if ($postThumbnailUrl)
             <div class="h-[360px] overflow-hidden w-full relative">
                 <div class="absolute w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out"></div>
-                <div class="absolute z-20 top-[15px] left-[15px] bg-secondary px-4 py-2 rounded-full text-black">Categorie</div>
+                @if (!empty($visibleElements) && in_array('category', $visibleElements))
+                    <div class="absolute z-20 top-[15px] left-[15px] bg-secondary px-4 py-2 rounded-full text-black">Categorie</div>
+                @endif
                 <img src="{{ $postThumbnailUrl }}" alt="Featured Image" class="w-full h-full object-cover object-center transform ease-in-out duration-300 group-hover:scale-110">
             </div>
         @endif
@@ -29,26 +31,26 @@
 
             <p class="font-bold text-lg">{{ $postTitle }}</p>
 
+            <div class="news-info">
+                @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($postSummary))
+                    <p class="mt-3 mb-2">{{ $postSummary }} </p>
+                @endif
 
-            @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($postSummary))
-                <p class="mt-3 mb-2">{{ $postSummary }} </p>
-            @endif
+                @if (!empty($visibleElements) && in_array('author', $visibleElements) && !empty($postAuthorName))
+                    <p class="text-gray-500">Geschreven door {{ $postAuthorName }}</p>
+                @endif
 
-            @if (!empty($visibleElements) && in_array('author', $visibleElements) && !empty($postAuthorName)))
-                <p class="text-gray-500">Geschreven door {{ $postAuthorName }}</p>
-            @endif
-
-            @if (!empty($visibleElements) && in_array('button', $visibleElements))
-                <div class="flex items-center flex-wrap">
-
-                    <a class="text-primary inline-flex items-center md:mb-2 lg:mb-0">Lees meer
-                        <svg class="w-4 h-4 ml-2 group-hover:translate-x-1 group-hover:scale-110 transition duration-300 ease-in-out" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M5 12h14"></path>
-                            <path d="M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
-                </div>
-            @endif
+                @if (!empty($visibleElements) && in_array('button', $visibleElements))
+                    <div class="mt-4 flex items-center flex-wrap">
+                        <a class="text-primary inline-flex items-center md:mb-2 lg:mb-0">Lees meer
+                            <svg class="w-4 h-4 ml-2 group-hover:translate-x-1 group-hover:scale-110 transition duration-300 ease-in-out" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M5 12h14"></path>
+                                <path d="M12 5l7 7-7 7"></path>
+                            </svg>
+                        </a>
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 </div>
