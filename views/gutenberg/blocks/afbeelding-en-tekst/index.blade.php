d52cf710913787b322fd30a2847fe81c1900600a
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-10-03 10:17:37 +0200

    Fall back borderRadius

--- views/gutenberg/blocks/afbeelding-en-tekst/index.blade.php
+++ views/gutenberg/blocks/afbeelding-en-tekst/index.blade.php
@@ -48,7 +48,7 @@
 
     // Theme settings
     $options = get_fields('option');
-    $borderRadius = $options['rounded_design'] ?? false ? $options['border_radius_strength'] ?? '' : 'rounded-none';
+    $borderRadius = $options['rounded_design'] === true ? ($options['border_radius_strength'] ?? '') : 'rounded-none';
 @endphp
 
 <section id="afbeelding-tekst-block" class="relative bg-{{ $backgroundColor}} py-16 lg:py-0"
