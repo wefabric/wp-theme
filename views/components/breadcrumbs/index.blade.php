1d9eef31160d45a2ebf94b3f89002d17a054456e
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-10-27 14:07:57 +0200

    Updated breadcrumbs

--- views/components/breadcrumbs/index.blade.php
+++ views/components/breadcrumbs/index.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full {{ $bg_color ?? 'bg-black text-white' }} py-10">
 	<div class="container mx-auto px-8 lg:px-24">
 		<div class="flex mx-auto text-base {{ $classes ?? 'w-4/5 lg:w-3/5' }}">
-
-			<div>
+			<div class="flex items-center">
+				<a href="/"><i class="fas fa-home mr-1"></i></a>
 				@php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); @endphp
 				@php if (function_exists('yoast_breadcrumb')) echo yoast_breadcrumb(); @endphp
 			</div>
