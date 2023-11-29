54e707cd47e283f32574d475a52f939cb076a1ed
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-11-29 12:43:32 +0100

    Updated logo block list-item

--- views/components/logos/list-item.blade.php
+++ views/components/logos/list-item.blade.php
@@ -9,22 +9,28 @@
     $logoSummary = get_the_excerpt($logo);
 @endphp
 
-<div class="logo-item">
-    @if ($logoUrl)<a href="{{ $logoUrl }}" target="_blank" class="group">@endif
+<div class="logo-item group h-full">
+    <div class="h-full flex flex-col @if($logoUrl) group-hover:-translate-y-4 duration-300 ease-in-out @endif">
+        <div class="overflow-hidden w-full relative rounded-{{ $borderRadius }}">
+            @if ($logoUrl)
+                <a href="{{ $logoUrl }}" target="_blank"
+                   class="absolute w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-30 transition-opacity duration-300 ease-in-out"></a>
+            @endif
             @if ($logoImage)
                 @include('components.image', [
                     'image_id' => $logoImage,
                     'size' => 'full',
                     'object_fit' => 'contain',
-                    'img_class' => 'w-full h-[200px] transition ease-in-out duration-300 group-hover:scale-105 rounded-' . $borderRadius,
+                    'img_class' => 'w-full h-[200px] transition ease-in-out duration-300 ' . ($logoUrl ? 'group-hover:scale-105 ' : '') . 'rounded-' . $borderRadius,
                     'alt' => $logoTitle
                 ])
             @endif
-            @if (!empty($visibleElements) && in_array('name', $visibleElements) && ($logoTitle))
-                <p class="group-hover:text-primary text-lg font-bold">{{ $logoTitle }}</p>
-            @endif
-            @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && ($logoSummary))
-                <p>{!! $logoSummary !!}</p>
-            @endif
-    @if ($logoUrl)</a>@endif
+        </div>
+        @if (!empty($visibleElements) && in_array('name', $visibleElements) && ($logoTitle))
+            <p class="text-lg font-bold @if($logoUrl) group-hover:text-primary @endif">{{ $logoTitle }}</p>
+        @endif
+        @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && ($logoSummary))
+            <p>{!! $logoSummary !!}</p>
+        @endif
+    </div>
 </div>
