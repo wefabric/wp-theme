46549629cfa97c7dca215231514a387d32b71cf7
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-10 21:27:58 +0100

    Updated news button margin

--- views/gutenberg/blocks/nieuws/index.blade.php
+++ views/gutenberg/blocks/nieuws/index.blade.php
@@ -102,7 +102,7 @@
                 @include('components.news.list', ['posts' => $posts])
             @endif
             @if (($button1Text) && ($button1Link))
-                <div class="w-full text-center mt-auto">
+                <div class="w-full text-center mt-4 md:mt-8">
                     @include('components.buttons.default', [
                        'text' => $button1Text,
                        'href' => $button1Link,
