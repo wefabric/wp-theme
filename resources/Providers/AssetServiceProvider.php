85f1e9d292c3798a518a15b5f45765d26b4c45a8
Leo Flapper <dominicus1991@gmail.com>
2023-12-12 21:03:56 +0100

    Fixed double loading of js and css

--- resources/Providers/AssetServiceProvider.php
+++ resources/Providers/AssetServiceProvider.php
@@ -21,28 +21,28 @@ class AssetServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        /** @var ThemeManager $theme */
-        $theme = $this->app->make('wp.theme');
-
-        /** For cache busting **/
-
-        $version = $theme->getHeader('version');
-        $manifestPath = get_template_directory().'/dist/mix-manifest.json';
-        if(file_exists($manifestPath)) {
-            $manifest = json_decode(file_get_contents($manifestPath), true);
-            preg_match('/id=(.*)/m',  $manifest['/js/app.js'] ?? $manifest['/js/css.js'], $matches);
-            if(isset($matches[1])) {
-                $version = $matches[1];
-            }
-        } else {
-            $version = substr(md5($theme->getUrl().microtime()), 0, 8);
-        }
-
-
-
-        Asset::add('theme_styles', 'css/app.css', [], $version)->to(['front', 'admin']);
-        Asset::add('admin_styles', 'css/admin.css', [], $version)->to(['admin']);
-        Asset::add('theme_woo', 'css/woocommerce.css', ['theme_styles'], $version)->to('front');
-        Asset::add('theme_js', 'js/app.js', [], $version)->to('front');
+//        /** @var ThemeManager $theme */
+//        $theme = $this->app->make('wp.theme');
+//
+//        /** For cache busting **/
+//
+//        $version = $theme->getHeader('version');
+//        $manifestPath = get_template_directory().'/dist/mix-manifest.json';
+//        if(file_exists($manifestPath)) {
+//            $manifest = json_decode(file_get_contents($manifestPath), true);
+//            preg_match('/id=(.*)/m',  $manifest['/js/app.js'] ?? $manifest['/js/css.js'], $matches);
+//            if(isset($matches[1])) {
+//                $version = $matches[1];
+//            }
+//        } else {
+//            $version = substr(md5($theme->getUrl().microtime()), 0, 8);
+//        }
+//
+//
+//
+//        Asset::add('theme_styles', 'css/app.css', [], $version)->to(['front', 'admin']);
+//        Asset::add('admin_styles', 'css/admin.css', [], $version)->to(['admin']);
+//        Asset::add('theme_woo', 'css/woocommerce.css', ['theme_styles'], $version)->to('front');
+//        Asset::add('theme_js', 'js/app.js', [], $version)->to('front');
     }
 }
