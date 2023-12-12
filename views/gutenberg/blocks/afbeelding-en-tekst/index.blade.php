c192ca7bc1d1149a123736588a57e9a17eec4ae5
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-12 16:20:42 +0100

    Update afbeedling tekst block

--- views/gutenberg/blocks/afbeelding-en-tekst/index.blade.php
+++ views/gutenberg/blocks/afbeelding-en-tekst/index.blade.php
@@ -1,116 +1,116 @@
-{{--@php--}}
-{{--    // Content--}}
-{{--    $title = $block['data']['title'] ?? '';--}}
-{{--    $titleColor = $block['data']['title_color'] ?? '';--}}
+@php
+    // Content
+    $title = $block['data']['title'] ?? '';
+    $titleColor = $block['data']['title_color'] ?? '';
 
-{{--    $text = $block['data']['text'] ?? '';--}}
-{{--    $textColor = $block['data']['text_color'] ?? '';--}}
-{{--    $textPosition = $block['data']['text_position'] ?? '';--}}
+    $text = $block['data']['text'] ?? '';
+    $textColor = $block['data']['text_color'] ?? '';
+    $textPosition = $block['data']['text_position'] ?? '';
 
-{{--    // Buttons--}}
-{{--    $button1Text = $block['data']['button_button_1']['title'] ?? '';--}}
-{{--    $button1Link = $block['data']['button_button_1']['url'] ?? '';--}}
-{{--    $button1Target = $block['data']['button_button_1']['target'] ?? '_self';--}}
-{{--    $button1Color = $block['data']['button_button_1_color'] ?? '';--}}
-{{--    $button1Style = $block['data']['button_button_1_style'] ?? '';--}}
-{{--    $button2Text = $block['data']['button_button_2']['title'] ?? '';--}}
-{{--    $button2Link = $block['data']['button_button_2']['url'] ?? '';--}}
-{{--    $button2Target = $block['data']['button_button_2']['target'] ?? '_self';--}}
-{{--    $button2Color = $block['data']['button_button_2_color'] ?? '';--}}
-{{--    $button2Style = $block['data']['button_button_2_style'] ?? '';--}}
+    // Buttons
+    $button1Text = $block['data']['button_button_1']['title'] ?? '';
+    $button1Link = $block['data']['button_button_1']['url'] ?? '';
+    $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
+    $button1Color = $block['data']['button_button_1_color'] ?? '';
+    $button1Style = $block['data']['button_button_1_style'] ?? '';
+    $button2Text = $block['data']['button_button_2']['title'] ?? '';
+    $button2Link = $block['data']['button_button_2']['url'] ?? '';
+    $button2Target = $block['data']['button_button_2']['target'] ?? '_self';
+    $button2Color = $block['data']['button_button_2_color'] ?? '';
+    $button2Style = $block['data']['button_button_2_style'] ?? '';
 
-{{--    $textOrder = $textPosition === 'left' ? 'lg:order-1' : 'lg:order-2';--}}
-{{--    $imageOrder = $textPosition === 'left' ? 'lg:order-2' : 'lg:order-1';--}}
+    $textOrder = $textPosition === 'left' ? 'lg:order-1' : 'lg:order-2';
+    $imageOrder = $textPosition === 'left' ? 'lg:order-2' : 'lg:order-1';
 
-{{--    $imageID = $block['data']['image'] ?? '';--}}
-{{--    $imageAlt = get_post_meta($imageID, '_wp_attachment_image_alt', true);--}}
-{{--    $imageSize = $block['data']['image_size'] ?? '';--}}
-{{--    $imageClass = '';--}}
-{{--    $textClass = '';--}}
+    $imageID = $block['data']['image'] ?? '';
+    $imageAlt = get_post_meta($imageID, '_wp_attachment_image_alt', true);
+    $imageSize = $block['data']['image_size'] ?? '';
+    $imageClass = '';
+    $textClass = '';
 
-{{--    if ($imageSize === '33') {--}}
-{{--        $imageClass = 'lg:w-1/3';--}}
-{{--        $textClass = 'lg:w-2/3';--}}
-{{--    } elseif ($imageSize === '50') {--}}
-{{--        $imageClass = 'lg:w-1/2';--}}
-{{--        $textClass = 'lg:w-1/2';--}}
-{{--    } elseif ($imageSize === '66') {--}}
-{{--        $imageClass = 'lg:w-2/3';--}}
-{{--        $textClass = 'lg:w-1/3';--}}
-{{--    }--}}
+    if ($imageSize === '33') {
+        $imageClass = 'lg:w-1/3';
+        $textClass = 'lg:w-2/3';
+    } elseif ($imageSize === '50') {
+        $imageClass = 'lg:w-1/2';
+        $textClass = 'lg:w-1/2';
+    } elseif ($imageSize === '66') {
+        $imageClass = 'lg:w-2/3';
+        $textClass = 'lg:w-1/3';
+    }
 
-{{--    $imageHeightClass = isset($block['data']['full_height']) && $block['data']['full_height'] ? 'h-full' : '';--}}
-{{--    $verticalCentered = $block['data']['vertical_centered'] ?? false;--}}
+    $imageHeightClass = isset($block['data']['full_height']) ? ($block['data']['full_height'] ? 'h-full' : '') : '';
+    $verticalCentered = $block['data']['vertical_centered'] ?? false;
 
-{{--    // Blokinstellingen--}}
-{{--    $blockWidth = $block['data']['block_width'] ?? 100;--}}
-{{--    $blockClassMap = [50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];--}}
-{{--    $blockClass = $blockClassMap[$blockWidth] ?? '';--}}
-{{--    $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';--}}
+    // Blokinstellingen
+    $blockWidth = $block['data']['block_width'] ?? 100;
+    $blockClassMap = [50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];
+    $blockClass = $blockClassMap[$blockWidth] ?? '';
+    $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';
 
-{{--    $backgroundColor = $block['data']['background_color'] ?? 'none';--}}
-{{--    $imageId = $block['data']['background_image'] ?? '';--}}
-{{--    $overlayEnabled = $block['data']['overlay_image'] ?? false;--}}
-{{--    $overlayColor = $block['data']['overlay_color'] ?? '';--}}
-{{--    $overlayOpacity = $block['data']['overlay_opacity'] ?? '';--}}
+    $backgroundColor = $block['data']['background_color'] ?? 'none';
+    $imageId = $block['data']['background_image'] ?? '';
+    $overlayEnabled = $block['data']['overlay_image'] ?? false;
+    $overlayColor = $block['data']['overlay_color'] ?? '';
+    $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
 
-{{--    $customBlockClasses = $block['data']['custom_css_classes'] ?? '';--}}
+    $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
 
-{{--    // Theme settings--}}
-{{--    $options = get_fields('option');--}}
-{{--    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength'] ?? '' : 'rounded-none';--}}
-{{--@endphp--}}
+    // Theme settings
+    $options = get_fields('option');
+    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength'] ?? '' : 'rounded-none';
+@endphp
 
-{{--<section id="afbeelding-tekst" class="relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"--}}
-{{--         style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \App\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">--}}
-{{--    @if ($overlayEnabled)--}}
-{{--        <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>--}}
-{{--    @endif--}}
-{{--    <div class="custom-styling relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">--}}
-{{--        <div class="{{ $blockClass }} mx-auto">--}}
-{{--            <div class="text-image flex flex-col lg:flex-row gap-8 xl:gap-20 @if ($verticalCentered) items-center @endif">--}}
-{{--                <div class="text {{ $textClass }} order-2 {{ $textOrder }}">--}}
-{{--                    @if ($title)--}}
-{{--                        <h2 class="mb-4 text-{{ $titleColor }}">{!! $title !!}</h2>--}}
-{{--                    @endif--}}
-{{--                    @if ($text)--}}
-{{--                        @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'text-' . $textColor])--}}
-{{--                    @endif--}}
-{{--                    @if (($button1Text) && ($button1Link))--}}
-{{--                        <div class="flex gap-4 mt-4 md:mt-8">--}}
-{{--                            @include('components.buttons.default', [--}}
-{{--                               'text' => $button1Text,--}}
-{{--                               'href' => $button1Link,--}}
-{{--                               'alt' => $button1Text,--}}
-{{--                               'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,--}}
-{{--                               'class' => 'rounded-lg',--}}
-{{--                               'target' => $button1Target,--}}
-{{--                           ])--}}
-{{--                            @if (($button2Text) && ($button2Link))--}}
-{{--                                @include('components.buttons.default', [--}}
-{{--                                   'text' => $button2Text,--}}
-{{--                                   'href' => $button2Link,--}}
-{{--                                   'alt' => $button2Text,--}}
-{{--                                   'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,--}}
-{{--                                   'class' => 'rounded-lg',--}}
-{{--                                   'target' => $button2Target,--}}
-{{--                               ])--}}
-{{--                            @endif--}}
-{{--                        </div>--}}
-{{--                    @endif--}}
-{{--                </div>--}}
-{{--                @if ($imageID)--}}
-{{--                    <div class="image {{ $imageClass }} order-1 {{ $imageOrder }}">--}}
-{{--                        @include('components.image', [--}}
-{{--                            'image_id' => $imageID,--}}
-{{--                            'size' => 'full',--}}
-{{--                            'object_fit' => 'cover',--}}
-{{--                            'img_class' => 'w-full object-cover rounded-' . $borderRadius . ' ' . $imageHeightClass,--}}
-{{--                            'alt' => $imageAlt--}}
-{{--                        ])--}}
-{{--                    </div>--}}
-{{--                @endif--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</section>--}}
+<section id="afbeelding-tekst" class="relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
+         style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \App\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">
+    @if ($overlayEnabled)
+        <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
+    @endif
+    <div class="custom-styling relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
+        <div class="{{ $blockClass }} mx-auto">
+            <div class="text-image flex flex-col lg:flex-row gap-8 xl:gap-20 @if ($verticalCentered) items-center @endif">
+                <div class="text {{ $textClass }} order-2 {{ $textOrder }}">
+                    @if ($title)
+                        <h2 class="mb-4 text-{{ $titleColor }}">{!! $title !!}</h2>
+                    @endif
+                    @if ($text)
+                        @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'text-' . $textColor])
+                    @endif
+                    @if (($button1Text) && ($button1Link))
+                        <div class="flex gap-4 mt-4 md:mt-8">
+                            @include('components.buttons.default', [
+                               'text' => $button1Text,
+                               'href' => $button1Link,
+                               'alt' => $button1Text,
+                               'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
+                               'class' => 'rounded-lg',
+                               'target' => $button1Target,
+                           ])
+                            @if (($button2Text) && ($button2Link))
+                                @include('components.buttons.default', [
+                                   'text' => $button2Text,
+                                   'href' => $button2Link,
+                                   'alt' => $button2Text,
+                                   'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
+                                   'class' => 'rounded-lg',
+                                   'target' => $button2Target,
+                               ])
+                            @endif
+                        </div>
+                    @endif
+                </div>
+                @if ($imageID)
+                    <div class="image {{ $imageClass }} order-1 {{ $imageOrder }}">
+                        @include('components.image', [
+                            'image_id' => $imageID,
+                            'size' => 'full',
+                            'object_fit' => 'cover',
+                            'img_class' => 'w-full object-cover rounded-' . $borderRadius . ' ' . $imageHeightClass,
+                            'alt' => $imageAlt
+                        ])
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</section>
