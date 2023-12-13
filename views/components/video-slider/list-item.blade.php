f66613a7c78b87aecd66312f1708c4a84f9f1415
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-13 10:28:30 +0100

    Full screen option for video slider items

--- views/components/video-slider/list-item.blade.php
+++ views/components/video-slider/list-item.blade.php
@@ -1,7 +1,7 @@
 @if ($video['url'])
     <div class="flex flex-col lg:flex-row items-center gap-x-8 gap-y-4">
         <div class="video-container aspect-video w-full lg:w-3/5">
-            <iframe width="100%" height="100%" src="{{ $video['url'] }}" title="YouTube video player" frameborder="0"></iframe>
+            <iframe width="100%" height="100%" src="{{ $video['url'] }}" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
         </div>
         @if ($video['caption'])
             <p class="h3 font-bold text-left w-full lg:w-2/5">{{ $video['caption'] }}</p>
