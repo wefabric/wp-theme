93ab755b3a0a1333cdb850ddd23a8a5f0f837d23
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-14 00:29:21 +0100

    Updated klantcase item

--- views/components/cases/list-item.blade.php
+++ views/components/cases/list-item.blade.php
@@ -20,61 +20,68 @@
 
 
 <div class="klantcase-item w-full text-{{ $caseTextColor }}">
+    <div class="relative flex h-full rounded-{{ $borderRadius }}">
+        <div class="absolute left-[30px] h-full top-0">
+            <div class="h-full py-12 flex flex-col items-center gap-4">
+                <div class="h6 text-quaternary-color vertical-text">Case</div>
+                <div class="h-full w-[1px] bg-quaternary-color"></div>
+            </div>
+        </div>
 
-
-    <div class="relative flex h-full bg-cta-color rounded-{{ $borderRadius }}">
-        <div class="absolute h-[50px] w-[1px] bg-quaternary left-[30px]">Case</div>
-
-        <div class="w-3/5 bg-background-color p-12">
-            <div class="w-full flex-col">
-                @if ($caseLogo)
-                    <div class="flex justify-end">
-                        @include('components.image', [
-                           'image_id' => $caseLogo,
-                           'size' => 'full',
-                           'object_fit' => 'contain',
-                           'img_class' => 'w-[120px] h-auto object-contain',
-                           'alt' => 'test'
-                       ])
+        <div class="flex w-3/5">
+            <div class="h-full flex flex-col flex-1 justify-start bg-background-color py-12 pl-24 pr-12">
+                <div class="flex flex-col justify-between h-full">
+                    <div class="flex justify-end logo">
+                        @if ($caseLogo)
+                            @include('components.image', [
+                                'image_id' => $caseLogo,
+                                'size' => 'full',
+                                'object_fit' => 'contain',
+                                'img_class' => 'w-[120px] h-auto object-contain',
+                                'alt' => 'test'
+                            ])
+                        @endif
                     </div>
-                @endif
-                @if($caseQuote)
-                    <div class="case-quote">
-                        <p class="text-[36px] text-{{ $caseQuoteColor }}">{!! $caseQuote !!}</p>
+                    <div class="case-content">
+                        @if($caseQuote)
+                            <div class="case-quote">
+                                <p class="quote-style mb-4 text-[36px] text-{{ $caseQuoteColor }}">{!! $caseQuote !!}</p>
+                            </div>
+                        @endif
+                        @if ($caseText)
+                            <div class="case-text">
+                                @include('components.content', ['content' => apply_filters('the_content', $caseText), 'class' => 'mb-6'])
+                            </div>
+                        @endif
+                        <div class="flex flex-col md:flex-row items-center gap-x-4 md:gap-x-6 gap-y-4">
+                            <!-- Add content for the flex container if needed -->
+                        </div>
+                        @if (!empty($visibleElements) && in_array('button', $visibleElements))
+                            @if ($buttonCardText)
+                                <div class="mt-4 z-10">
+                                    @include('components.buttons.default', [
+                                        'text' => $buttonCardText,
+                                        'href' => $caseUrl,
+                                        'alt' => $buttonCardText,
+                                        'colors' => 'btn-' . $buttonCardColor . ' btn-' . $buttonCardStyle,
+                                        'class' => 'rounded-lg',
+                                    ])
+                                </div>
+                            @endif
+                        @endif
                     </div>
-                @endif
-
-                @if ($caseText)
-                    <div class="case-text">
-                        @include('components.content', ['content' => apply_filters('the_content', $caseText), 'class' => 'mb-6'])
-                    </div>
-                @endif
-                <div class="flex flex-col md:flex-row items-center gap-x-4 md:gap-x-6 gap-y-4">
                 </div>
             </div>
-            @if (!empty($visibleElements) && in_array('button', $visibleElements))
-                @if ($buttonCardText)
-                    <div class="mt-auto pt-8 z-10">
-                        @include('components.buttons.default', [
-                           'text' => $buttonCardText,
-                           'href' => $caseUrl,
-                           'alt' => $buttonCardText,
-                           'colors' => 'btn-' . $buttonCardColor . ' btn-' . $buttonCardStyle,
-                           'class' => 'rounded-lg',
-                       ])
-                    </div>
-                @endif
-            @endif
         </div>
         <div class="w-2/5">
             @if ($caseImage)
                 @include('components.image', [
-                   'image_id' => $caseImage,
-                   'size' => 'full',
-                   'object_fit' => 'cover',
-                   'img_class' => 'w-full max-h-[400px] md:max-h-[600px] object-cover rounded-r-' . $borderRadius,
-                   'alt' => 'test'
-               ])
+                    'image_id' => $caseImage,
+                    'size' => 'full',
+                    'object_fit' => 'cover',
+                    'img_class' => 'w-full max-h-[600px] object-cover rounded-r-' . $borderRadius,
+                    'alt' => 'test'
+                ])
             @endif
         </div>
     </div>
