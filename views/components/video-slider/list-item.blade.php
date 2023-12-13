17ef21bc695e3f24a42fdb48d6d95dddf910d4e6
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-13 23:17:12 +0100

    Centered video in video slider block if no caption

--- views/components/video-slider/list-item.blade.php
+++ views/components/video-slider/list-item.blade.php
@@ -1,5 +1,5 @@
 @if ($video['url'])
-    <div class="flex flex-col lg:flex-row items-center gap-x-8 gap-y-4">
+    <div class="flex flex-col lg:flex-row items-center gap-x-8 gap-y-4 @if(!$video['caption']) justify-center @endif">
         <div class="video-container aspect-video w-full lg:w-3/5">
             <iframe width="100%" height="100%" src="{{ $video['url'] }}" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
         </div>
