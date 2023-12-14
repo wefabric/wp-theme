9cb7b91617f6620b669d0490e0e9423d7d6d595d
ErikWefabric <141040262+ErikWefabric@users.noreply.github.com>
2023-12-14 11:04:57 +0100

    added login btn to pre-header

--- views/components/navigation/secondary-nav.blade.php
+++ views/components/navigation/secondary-nav.blade.php
@@ -8,12 +8,12 @@
 @endphp
 
 <div class="secondary-navigation hidden lg:block w-full bg-{{ $options['secondary_menu_background_color'] ?? 'primary-color' }}">
-    <div class="flex items-center justify-between flex-row container mx-auto py-2.5 px-4">
+    <div class="flex items-center justify-between flex-row container mx-auto h-12 px-4">
         @if(isset($options['secondary_menu_text']))
-            <div class="font-bold text-sm text-{{ $options['secondary_menu_text_color'] ?? 'white' }}">{{ $options['secondary_menu_text'] }}</div>
+            <div class="font-normal text-sm text-{{ $options['secondary_menu_text_color'] ?? 'white' }}">{{ $options['secondary_menu_text'] }}</div>
         @endif
         @if (!empty($options['secondary_menu_show_elements']))
-            <div class="flex gap-4 text-sm text-{{ $options['secondary_menu_text_color'] ?? 'white' }}">
+            <div class="flex gap-4 text-sm h-full text-{{ $options['secondary_menu_text_color'] ?? 'white' }}">
                 @foreach($footer_establishments as $key => $establishment_config)
                     @php
                         $establishment = $establishment_config ? new \Wefabric\WPEstablishments\Establishment($establishment_config['establishment']) : null;
@@ -21,14 +21,21 @@
                         $email = $establishment ? $establishment->getContactEmailAddress() : '';
                     @endphp
                     @if (in_array('phone', $options['secondary_menu_show_elements']))
-                        <a class="group flex items-center gap-2" href="tel:{{ $phone }}" alt="Telefoonnummer">
-                            <i class="p-1.5 flex justify-center items-center bg-primary-light group-hover:bg-primary-dark rounded-lg fa-solid fa-phone"></i>
-                            <span class="align-middle">{{ $phone }}</span>
+                        <a class="group flex items-center gap-2" href="tel:{{ $phone }} " title="Telefoonnummer">
+                            <i class="p-1.5 flex text-md justify-center items-center  rotate-[270deg] rounded-lg fa-solid fa-phone"></i>
+                            <span class="align-middle ">{{ $phone }}</span>
                         </a>
                     @endif
+
+                    <a class="group flex items-center gap-2 bg-primary rounded-b-md p-4 h-5/6" target="_blank"
+                       href="https://kms3.zijlstraberoepskleding.nl/u/inloggen" title="Inloggen KMS">
+                        <i class="p-1.5 flex text-md justify-center items-center font-dyno rounded-lg fa-solid fa-user"></i>
+                        <span class="align-middle text-white">Inloggen KMS</span>
+                    </a>
+
                     @if (in_array('email', $options['secondary_menu_show_elements']))
-                        <a class="group flex items-center gap-2" href="mailto:{{ $email }}" alt="E-mailadres">
-                            <i class="p-1.5 flex justify-center items-center bg-primary-light group-hover:bg-primary-dark rounded-lg fa-solid fa-envelope"></i>
+                        <a class="group flex items-center gap-2" href="mailto:{{ $email }}" title="E-mailadres">
+                            <i class="p-1.5 flex justify-center items-center  rounded-lg fa-solid fa-envelope"></i>
                             <span class="align-middle">{{ $email }}</span>
                         </a>
                     @endif
