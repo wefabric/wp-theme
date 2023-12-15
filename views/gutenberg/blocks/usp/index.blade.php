81e6e90fa99b318d74cfc3dca37ea3eab212b659
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-15 20:20:21 +0100

    Updated USP custom-styling

--- views/gutenberg/blocks/usp/index.blade.php
+++ views/gutenberg/blocks/usp/index.blade.php
@@ -62,7 +62,7 @@
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
     <div class="relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
-        <div class="{{ $blockClass }} mx-auto relative">
+        <div class="custom-styling {{ $blockClass }} mx-auto relative">
             @if ($title)
                 <h2 class="container mx-auto lg:mb-8 @if($blockWidth == 'fullscreen') px-8 @endif {{ $titleClass }} text-{{ $titleColor }}">{!! $title !!}</h2>
             @endif
