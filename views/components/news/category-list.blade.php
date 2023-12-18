4b3877bedbfe47370ca0a180a075f5b885c468f9
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-18 15:33:23 +0100

    Updated category list

--- views/components/news/category-list.blade.php
+++ views/components/news/category-list.blade.php
@@ -11,7 +11,7 @@
             @php
                 $categoryColor = get_field('category_color', 'category_' . $category->term_id);
             @endphp
-            <a href="{{ get_category_link($category) }}" style="background-color: {{ $categoryColor }}" class="@if(empty($categoryColor)) bg-primary hover:bg-primary-dark @endif text-white px-4 py-2 rounded-full">{{ $category->name }}</a>
+            <a href="{{ get_category_link($category) }}" style="background-color: {{ $categoryColor }}" class="@if(empty($categoryColor)) bg-primary hover:bg-primary-dark @endif text-white px-4 py-2 rounded-full">{!! $category->name !!}</a>
         @endforeach
     </div>
 </div>
