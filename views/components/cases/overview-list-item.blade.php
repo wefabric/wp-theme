18310a37d6816b40b03b33955daa7a30d0ba7741
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-18 22:23:45 +0100

    Updated featured klantcase item styling

--- views/components/cases/overview-list-item.blade.php
+++ views/components/cases/overview-list-item.blade.php
@@ -23,9 +23,9 @@
         <a href="{{ $caseUrl }}" aria-label="Ga naar {{ $caseTitle }} pagina"
            class="card-overlay top-0 left-0 absolute h-full w-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out rounded-{{ $borderRadius }}"></a>
 
-        <div class="absolute bottom-0 w-full opacity-50 group-hover:opacity-0 transition-opacity duration-300 ease-in-out h-1/3 bg-primary rounded-b-{{ $borderRadius }}"></div>
+        <div class="absolute bottom-0 w-full opacity-50 group-hover:opacity-0 transition-opacity duration-300 ease-in-out h-3/5 sm:h-1/2 lg:h-1/3 bg-primary rounded-b-{{ $borderRadius }}"></div>
         @if($caseExcerpt)
-            <div class="hidden lg:block absolute z-20 -translate-x-1/2 -translate-y-2/3 left-1/2 top-1/2 opacity-0 group-hover:opacity-100 h5 transition-opacity duration-300 ease-in-out">{{ $caseExcerpt }}</div>
+            <div class="hidden lg:block absolute z-20 -translate-x-1/2 -translate-y-full left-1/2 top-1/2 opacity-0 group-hover:opacity-100 h5 transition-opacity duration-300 ease-in-out">{{ $caseExcerpt }}</div>
         @endif
 
         <a href="{{ $caseUrl }} " aria-label="Ga naar {{ $caseTitle }} pagina"
