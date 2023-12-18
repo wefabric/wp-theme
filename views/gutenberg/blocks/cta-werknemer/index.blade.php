755eafdd552852b9b249717421a52f45113693bf
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-18 13:54:39 +0100

    Updated CTA blocks layout

--- views/gutenberg/blocks/cta-werknemer/index.blade.php
+++ views/gutenberg/blocks/cta-werknemer/index.blade.php
@@ -4,9 +4,14 @@
     $titleColor = $block['data']['title_color'] ?? '';
     $text = $block['data']['text'] ?? '';
     $textColor = $block['data']['text_color'] ?? '';
-//    $textPosition = $block['data']['text_position'] ?? '';
-//    $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
-//    $textClass = $titleClassMap[$textPosition] ?? '';
+
+    $textPosition = $block['data']['text_position'] ?? '';
+    $textClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
+    $textClass = $textClassMap[$textPosition] ?? 'text-center';
+    $flexClassMap = ['left' => 'justify-start', 'center' => 'justify-center', 'right' => 'justify-end',];
+    $flexClass = $flexClassMap[$textPosition] ?? 'items-center';
+    $ctaLayout = $block['data']['cta_layout'] ?? '';
+    $flexDirection = ($ctaLayout === 'vertical') ? 'flex-col' : (($ctaLayout === 'horizontal') ? 'flex-row' : '');
 
     $ctaForm = $block['data']['form'] ?? '';
     $blockBackgroundColor = $block['data']['block_background_color'] ?? '';
@@ -64,7 +69,7 @@
             <div class="bg-{{ $blockBackgroundColor }} w-full h-full"></div>
         </div>
 
-        @if ($employeeImage)
+        @if ($employeeImage && $ctaLayout == 'vertical')
             <div class="overlay absolute z-30 left-1/2 -translate-x-1/2 -translate-y-1/2">
                 @include('components.image', [
                  'image_id' => $employeeImageId,
@@ -82,41 +87,57 @@
                     <div class="absolute inset-0 @if($blockWidth !== 'fullscreen') md:mx-8 @endif bg-{{ $blockOverlayColor }} opacity-{{ $blockOverlayOpacity }}"></div>
                 @endif
 
-                <div class="container mx-auto @if($blockWidth == 'fullscreen') md:px-8 @else w-full xl:w-2/3 @endif relative z-10 ">
-                    <div class="w-full text-center mx-auto @if($employeeImage) mt-16 md:mt-32 @endif">
-                        @if ($title)
-                            <h2 class="text-{{ $titleColor }}">{!! $title !!}</h2>
-                        @endif
-                        @if ($text)
-                            @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'mt-4 md:mt-4 text-' . $textColor])
-                        @endif
-                        @if (($button1Text) && ($button1Link))
-                            <div class="flex gap-4 w-full justify-center mt-4 md:mt-8">
-                                @include('components.buttons.default', [
-                                   'text' => $button1Text,
-                                   'href' => $button1Link,
-                                   'alt' => $button1Text,
-                                   'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                                   'class' => 'rounded-lg',
-                                   'target' => $button1Target,
-                               ])
-                                @if (($button2Text) && ($button2Link))
+                <div class="container mx-auto @if($blockWidth == 'fullscreen') md:px-8 @else w-full @endif relative z-10">
+                    <div class="flex flex-col lg:{{ $flexDirection }} {{ $flexClass }} justify-center sm:px-8 lg:px-32 gap-y-4 gap-x-8 @if($employeeImage && $ctaLayout == 'vertical') mt-12 sm:mt-16 lg:mt-32 @endif">
+
+                        <div class="@if ($ctaLayout == 'horizontal') order-2 lg:order-1 @else order-2 @endif @if($employeeImage) w-full lg:w-4/5 text-center lg:{{$textClass}} @else w-full lg:w-full {{ $textClass }} @endif">
+                            @if ($title)
+                                <h2 class="text-{{ $titleColor }}">{!! $title !!}</h2>
+                            @endif
+                            @if ($text)
+                                @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'mt-4 md:mt-4 text-' . $textColor])
+                            @endif
+                            @if (($button1Text) && ($button1Link))
+                                <div class="flex gap-4 w-full mt-4 md:mt-8 justify-center lg:{{ $flexClass }}">
                                     @include('components.buttons.default', [
-                                       'text' => $button2Text,
-                                       'href' => $button2Link,
-                                       'alt' => $button2Text,
-                                       'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
+                                       'text' => $button1Text,
+                                       'href' => $button1Link,
+                                       'alt' => $button1Text,
+                                       'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
                                        'class' => 'rounded-lg',
-                                       'target' => $button2Target,
+                                       'target' => $button1Target,
                                    ])
-                                @endif
+                                    @if (($button2Text) && ($button2Link))
+                                        @include('components.buttons.default', [
+                                           'text' => $button2Text,
+                                           'href' => $button2Link,
+                                           'alt' => $button2Text,
+                                           'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
+                                           'class' => 'rounded-lg',
+                                           'target' => $button2Target,
+                                       ])
+                                    @endif
+                                </div>
+                            @endif
+                            @if ($ctaForm)
+                                <div class="w-full mx-auto mt-10 text-left text-white">
+                                    {!! gravity_form($ctaForm, false) ; !!}
+                                </div>
+                            @endif
+                        </div>
+
+                        @if ($ctaLayout == 'horizontal' && $employeeImage)
+                            <div class="@if($ctaLayout == 'horizontal') order-1 lg:order-2 @else order-1 @endif w-full lg:w-auto -mt-[150px]">
+                                @include('components.image', [
+                                    'image_id' => $employeeImageId,
+                                    'size' => 'full',
+                                    'object_fit' => 'cover',
+                                    'img_class' => 'h-[200px] lg:h-auto w-auto lg:w-[350px] object-contain mx-auto rounded-full',
+                                    'alt' => $employeeTitle,
+                                ])
                             </div>
                         @endif
-                        @if ($ctaForm)
-                            <div class="w-full lg:w-2/3 xl:w-1/2 mx-auto mt-10 text-left text-white">
-                                {!! gravity_form($ctaForm, false) ; !!}
-                            </div>
-                        @endif
+
                     </div>
                 </div>
             </div>
