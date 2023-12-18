a8113800a5cc3f5ec58cffaefc78221685414b44
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-18 15:42:25 +0100

    Update mailchimp.blade.php

--- views/components/footer/mailchimp.blade.php
+++ views/components/footer/mailchimp.blade.php
@@ -10,7 +10,7 @@
 			<input type="text" name="b_97458c1812b52842329db8e54_5af5026eaf" tabindex="-1" value="">
 		</div>
 		<div class="flex align-center">
-			<button type="submit" class="h-full w-10 btn-white text-primary hover:text-white text-center text-sm rounded-r-lg">
+			<button type="submit" class="mailchimp-submit h-full w-10 btn-white text-primary hover:text-white text-center text-sm rounded-r-lg">
 				<i class="fa-solid fa-paper-plane "></i>
 				<span class="screen-reader-only">Aanmelden</span>
 			</button>
