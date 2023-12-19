408a30233a5926cb5579724909b2ba23ef058bb0
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2023-12-19 09:45:58 +0100

    Updated news page

--- views/archive/blog.blade.php
+++ views/archive/blog.blade.php
@@ -16,32 +16,33 @@
 {{--		@include('components.news.category-links')--}}
 {{--	</div>--}}
 
-	<section class="news-archive-grid relative">
-		<div class="container mx-auto my-12 lg:my-12">
-			<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
-				@loop
-					@include('components.cards.newsitem', [
-						'item' => get_the_ID()
-					])
-				@endloop
-			</div>
+{{--	<section class="news-archive-grid relative">--}}
+{{--		<div class="container mx-auto my-12 lg:my-12">--}}
+{{--			<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">--}}
+{{--				@loop--}}
+{{--					@include('components.cards.newsitem', [--}}
+{{--						'item' => get_the_ID()--}}
+{{--					])--}}
+{{--				@endloop--}}
+{{--			</div>--}}
 
-			<div class="pagination text-center mt-12 lg:mt-24">
-				@php
-					the_posts_pagination( [
-						'mid_size'  => 1,
-						'prev_next' => false,
-//						'prev_text' => __( '<', 'textdomain' ),
-//						'next_text' => __( '>', 'textdomain' ),
-					]);
-				@endphp
-			</div>
-		</div>
-{{--
-		<div class="bg-primary absolute w-full h-96 bottom-0 left-0 -z-50">
-			//bottom colored bar.
-		</div>
---}}
-	</section>
+{{--			<div class="pagination text-center mt-12 lg:mt-24">--}}
+{{--				@php--}}
+{{--					the_posts_pagination( [--}}
+{{--						'mid_size'  => 1,--}}
+{{--						'prev_next' => false,--}}
+{{--//						'prev_text' => __( '<', 'textdomain' ),--}}
+{{--//						'next_text' => __( '>', 'textdomain' ),--}}
+{{--					]);--}}
+{{--				@endphp--}}
+{{--			</div>--}}
+{{--		</div>--}}
+{{----}}
+{{--    Hide this--}}
+{{--		<div class="bg-primary absolute w-full h-96 bottom-0 left-0 -z-50">--}}
+{{--			//bottom colored bar.--}}
+{{--		</div>--}}
+{{----}}
+{{--	</section>--}}
 
 @endsection
