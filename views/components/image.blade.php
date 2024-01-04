8f7890439e251e3b797ff64bf67befb7fc5b4797
Leo Flapper <dominicus1991@gmail.com>
2024-01-04 16:53:24 +0100

    Fix for image component to check if image id is not null

--- views/components/image.blade.php
+++ views/components/image.blade.php
@@ -1,8 +1,10 @@
-<span class="image {{ $class ?? '' }}">
-    {!!
-        wp_get_attachment_image($image_id, $size ?? 'full', 'false', [
-	        'class' => 'object-'. ($object_fit ?? 'cover') .' '. ($img_class ?? ''). ' ',
-	        'alt' => $alt ?? '',
-        ])
-    !!}
-</span>
+@if($image_id)
+    <span class="image {{ $class ?? '' }}">
+        {!!
+            wp_get_attachment_image($image_id, $size ?? 'full', 'false', [
+                'class' => 'object-'. ($object_fit ?? 'cover') .' '. ($img_class ?? ''). ' ',
+                'alt' => $alt ?? '',
+            ])
+        !!}
+    </span>
+@endif
