704b4a317cfb4869baf85d87af5847171de5ca56
Leo Flapper <dominicus1991@gmail.com>
2024-01-08 08:44:21 +0100

    Moved dynamic content calling to dom content loaded

--- assets/js/dynamic-content.js
+++ assets/js/dynamic-content.js
@@ -1,53 +1,55 @@
-jQuery.ajax({
-    url: '/dynamic-content' ,
-    type: 'get',
-    dataType: 'json',
-    success: function (response) {
-        if(typeof response.csrf !== 'undefined') {
-            $('meta[name=csrf-token]').attr('content', response.csrf);
-            $('input[name=_token]').val(response.csrf);
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    jQuery.ajax({
+        url: '/dynamic-content',
+        type: 'get',
+        dataType: 'json',
+        success: function (response) {
+            if (typeof response.csrf !== 'undefined') {
+                $('meta[name=csrf-token]').attr('content', response.csrf);
+                $('input[name=_token]').val(response.csrf);
+            }
 
-        if(typeof response.cart !== 'undefined') {
+            if (typeof response.cart !== 'undefined') {
 
-            if(typeof response.cart.items_count !== 'undefined') {
-                var cartItemsAmount = parseInt(response.cart.items_count);
-                if(cartItemsAmount > 0) {
-                    $('.cart-items-count').show().html(cartItemsAmount);
+                if (typeof response.cart.items_count !== 'undefined') {
+                    var cartItemsAmount = parseInt(response.cart.items_count);
+                    if (cartItemsAmount > 0) {
+                        $('.cart-items-count').show().html(cartItemsAmount);
+                    }
+
                 }
 
+                if (typeof response.cart.amount !== 'undefined') {
+                    $('.cart-amount').html(response.cart.amount);
+                }
             }
 
-            if(typeof response.cart.amount !== 'undefined') {
-                $('.cart-amount').html(response.cart.amount);
+            if (typeof response.out_of_office !== 'undefined' && response.out_of_office) {
+                $('body').append(response.out_of_office);
+
+                if (sessionStorage.getItem("outOfOfficeStatus") !== "closed") {
+                    $("#popup").delay(500).fadeIn();
+                }
+
+                $('#popup-close').click(function (e) {
+                    $('#popup').fadeOut();
+                    sessionStorage.setItem("outOfOfficeStatus", "closed")
+                });
             }
-        }
 
-        if(typeof response.out_of_office !== 'undefined' && response.out_of_office) {
-            $('body').append(response.out_of_office);
-
-            if(sessionStorage.getItem("outOfOfficeStatus") !== "closed") {
-                $("#popup").delay(500).fadeIn();
-            }
-
-            $('#popup-close').click(function(e) {
-                $('#popup').fadeOut();
-                sessionStorage.setItem("outOfOfficeStatus", "closed")
-            });
-        }
-
-    },
-});
+        },
+    });
 
 
 // Get all the forms on the page
-let allForms = document.querySelectorAll("form");
+    let allForms = document.querySelectorAll("form");
 
 // Loop through all forms and add hidden input fields
-for (var i = 0; i < allForms.length; i++) {
-    let hiddenInput = document.createElement("input");
-    hiddenInput.type = "hidden";
-    hiddenInput.name = '_token';
-    hiddenInput.value = $('meta[name=csrf-token]').attr('content');
-    allForms[i].appendChild(hiddenInput);
-}
+    for (var i = 0; i < allForms.length; i++) {
+        let hiddenInput = document.createElement("input");
+        hiddenInput.type = "hidden";
+        hiddenInput.name = '_token';
+        hiddenInput.value = $('meta[name=csrf-token]').attr('content');
+        allForms[i].appendChild(hiddenInput);
+    }
+});
