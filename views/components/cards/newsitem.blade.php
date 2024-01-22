f94241e9ac536024f154183a8f30dc62ca206b37
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-01-22 15:37:31 +0100

    Updated news item card

--- views/components/cards/newsitem.blade.php
+++ views/components/cards/newsitem.blade.php
@@ -2,6 +2,7 @@
     if(! isset($postId)) {
         $postId = $item; //in case of slider, or news page
     }
+
     $fields = get_fields($postId);
     $service = get_post($postId);
 
@@ -17,67 +18,6 @@
     $options = get_fields('option');
     $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength']??'': 'rounded-none';
 @endphp
-
-{{--<div onclick="window.location.href = '{{ get_permalink($postId) }}'" class="cursor-pointer card hover:shadow-3xl max-w-md {{ (isset($lg_hidden) && $lg_hidden) ? 'hidden lg:flex' : 'flex' }} flex-col mx-auto h-full relative ">--}}
-{{--    <div class="mx-auto mb-5 w-full rounded-lg">--}}
-{{--        @include('components.image', [--}}
-{{--            'image_id' => get_post_thumbnail_id($postId),--}}
-{{--			'size' => 'news-thumbnail',--}}
-{{--			'class' => 'disable-rounded',--}}
-{{--            'img_class' => 'rounded-t-lg w-full max-h-[200px]',--}}
-{{--        ])--}}
-{{--    </div>--}}
-
-{{--    <div class="px-7">--}}
-{{--        <div class="card-category-title leading-relaxed">--}}
-{{--            {{ $categories[0]->name }}--}}
-{{--        </div>--}}
-
-{{--        <div class="">--}}
-{{--            @include('components.headings.normal', [--}}
-{{--                'type' => 'h3',--}}
-{{--				'display' => 'h4',--}}
-{{--                'heading' => get_the_title($postId),--}}
-{{--            ])--}}
-{{--        </div>--}}
-
-{{--        <div class="pb-16">--}}
-{{--            @include('components.content', [--}}
-{{--                'content' => get_the_excerpt($postId),--}}
-{{--            ])--}}
-{{--        </div>--}}
-
-{{--        <div class="absolute bottom-0 right-5 w-full pb-5">--}}
-{{--            <div class="text-right text-base text-bold">--}}
-{{--                @include('components.link.opening', [--}}
-{{--                    'href' => get_permalink($postId),--}}
-{{--                    'alt' => 'Lees meer',--}}
-{{--                    'class' => 'no-underline group',--}}
-{{--                ])--}}
-{{--					<span class="group-hover:underline">Lees meer</span>--}}
-{{--					<i class="fa-solid fa-circle-caret-right ml-2"></i>--}}
-{{--                @include('components.link.closing')--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-
-
-{{--@php--}}
-{{--    $fields = get_fields($post);--}}
-{{--    $postThumbnailId = get_post_thumbnail_id($post);--}}
-{{--    $postTitle = get_the_title($post);--}}
-{{--    $postUrl = get_permalink($post);--}}
-
-{{--    // Weergave--}}
-{{--    $visibleElements = $block['data']['show_element'] ?? [];--}}
-{{--    $postSummary = get_the_excerpt($post);--}}
-{{--    $postDate = get_the_date('j F, Y', $post);--}}
-{{--    $postAuthorId = get_post_field('post_author', $post);--}}
-{{--    $postAuthorName = get_the_author_meta('display_name', $postAuthorId);--}}
-{{--    $postCategories = get_the_category($post);--}}
-{{--@endphp--}}
-
 
 <div class="nieuws-item group h-full">
     <div class="h-full flex flex-col group-hover:-translate-y-4 duration-300 ease-in-out">
@@ -99,7 +39,6 @@
                     @endforeach
                 @endif
             </div>
-
 
             @include('components.image', [
                 'image_id' => get_post_thumbnail_id($postId),
