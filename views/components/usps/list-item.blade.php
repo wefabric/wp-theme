7b2ff5deaee05ba76e9f0de339b6ce084a3ec4c3
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-02-20 17:15:10 +0100

    Updated usp and cta acf files

--- views/components/usps/list-item.blade.php
+++ views/components/usps/list-item.blade.php
@@ -4,7 +4,8 @@
     $icon = $usp['uspIcon'];
     $iconColor = $usp['uspIconColor'];
     $imageID = $usp['uspImage'];
-    $textColor = $block['data']['text_color'] ?? '';
+    $uspTitleColor = $block['data']['usp_title_color'] ?? '';
+    $uspTextColor = $block['data']['usp_text_color'] ?? '';
     $altText = get_post_meta($imageID, '_wp_attachment_image_alt', true) ?: 'usp-image';
 @endphp
 
@@ -26,10 +27,10 @@
         @if ($title || $text)
             <div class="usp-data">
                 @if ($title)
-                    <p class="usp-title text-{{$textColor}} font-bold h4">{!! $title !!}</p>
+                    <p class="usp-title text-{{ $uspTitleColor }} font-bold h4">{!! $title !!}</p>
                 @endif
                 @if ($text)
-                    @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'mt-4 text-' . $textColor])
+                    @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'mt-4 text-' . $uspTextColor])
                 @endif
             </div>
         @endif
