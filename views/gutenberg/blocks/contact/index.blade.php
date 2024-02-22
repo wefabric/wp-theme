00f3a9ef5c36c25299871f9a46dc3c48b6be6d8a
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-02-22 16:32:51 +0100

    Added styling classes for contact block

--- views/gutenberg/blocks/contact/index.blade.php
+++ views/gutenberg/blocks/contact/index.blade.php
@@ -41,37 +41,39 @@
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
     <div class="relative z-10 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
-        <div class="{{ $blockClass }} mx-auto flex flex-col lg:flex-row gap-8">
+        <div class="custom-layout {{ $blockClass }} mx-auto flex flex-col lg:flex-row gap-8">
             <div class="content-block w-full lg:w-1/3 order-1 lg:order-0 px-8">
                 @if ($title)
                     <h2 class="mb-4 text-{{ $titleColor }}">{!! $title !!}</h2>
                 @endif
                 @if (!empty($visibleElements) && in_array('establishments', $visibleElements) && $establishment_query->have_posts())
-                    @while ($establishment_query->have_posts())
-                        @php
-                            $establishment_query->the_post();
-                            $establishment_title = get_the_title();
-                            $establishment_id = get_the_ID();
-                            $street = get_post_meta($establishment_id, 'street', true);
-                            $zipcode = get_post_meta($establishment_id, 'postcode', true);
-                            $house_number = get_post_meta($establishment_id, 'house_number', true);
-                            $house_number_addition = get_post_meta($establishment_id, 'house_number_addition', true);
-                            $city = get_post_meta($establishment_id, 'city', true);
-                        @endphp
-                        <div class="mb-4 text-{{ $textColor }}">
-                            <p class="font-bold">{{ $establishment_title }}</p>
-                            @if($street && $house_number)
-                                <p>{{ $street }} {{ $house_number }} {{ $house_number_addition }}</p>
-                            @endif
-                            @if($zipcode && $city)
-                                <p>{{ $zipcode }}, {{ $city }}</p>
-                            @endif
-                            @if($street && $visibleElements && in_array('route_description', $visibleElements))
-                                <a href="https://www.google.com/maps/search/?api=1&query={{ $street }}+{{ $house_number }}{{ $house_number_addition }}+{{ $zipcode }}+{{ $city }}"
-                                   target="_blank" class="hover:text-primary underline">Routebeschrijving ></a>
-                            @endif
-                        </div>
-                    @endwhile
+                    <div class="establishment-list">
+                        @while ($establishment_query->have_posts())
+                            @php
+                                $establishment_query->the_post();
+                                $establishment_title = get_the_title();
+                                $establishment_id = get_the_ID();
+                                $street = get_post_meta($establishment_id, 'street', true);
+                                $zipcode = get_post_meta($establishment_id, 'postcode', true);
+                                $house_number = get_post_meta($establishment_id, 'house_number', true);
+                                $house_number_addition = get_post_meta($establishment_id, 'house_number_addition', true);
+                                $city = get_post_meta($establishment_id, 'city', true);
+                            @endphp
+                            <div class="establishment mb-4 text-{{ $textColor }}">
+                                <p class="establishment-title font-bold">{{ $establishment_title }}</p>
+                                @if($street && $house_number)
+                                    <p class="establishment-street">{{ $street }} {{ $house_number }} {{ $house_number_addition }}</p>
+                                @endif
+                                @if($zipcode && $city)
+                                    <p class="establishment-zipcode">{{ $zipcode }}, {{ $city }}</p>
+                                @endif
+                                @if($street && $visibleElements && in_array('route_description', $visibleElements))
+                                    <a href="https://www.google.com/maps/search/?api=1&query={{ $street }}+{{ $house_number }}{{ $house_number_addition }}+{{ $zipcode }}+{{ $city }}"
+                                       target="_blank" class="hover:text-primary underline">Routebeschrijving ></a>
+                                @endif
+                            </div>
+                        @endwhile
+                    </div>
                 @endif
                 @if ($text)
                     @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'mt-4 text-' . $textColor])
