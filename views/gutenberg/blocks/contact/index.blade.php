76e5591ef25c6fa5c9801c2646fcc795332e0b36
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-02-22 16:54:40 +0100

    Update index.blade.php

--- views/gutenberg/blocks/contact/index.blade.php
+++ views/gutenberg/blocks/contact/index.blade.php
@@ -82,7 +82,9 @@
             @if ($form)
                 <div class="form-block w-full lg:w-2/3 order-0 lg:order-1 sm:px-8">
                     <div class="contact-block bg-{{ $formBackgroundColor }} p-12 lg:shadow-xl rounded-{{ $borderRadius }}">
-                        {!! gravity_form($form) !!}
+                        <div class="form h-full w-full">
+                            {!! gravity_form($form) !!}
+                        </div>
                     </div>
                 </div>
             @endif
