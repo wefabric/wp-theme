7de2b139012adbc555cfb44648531207561fd1b3
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-02-22 11:36:24 +0100

    Updated CTA blocks

--- views/gutenberg/blocks/cta-button/index.blade.php
+++ views/gutenberg/blocks/cta-button/index.blade.php
@@ -104,7 +104,7 @@
                                    'href' => $button1Link,
                                    'alt' => $button1Text,
                                    'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                                   'class' => 'rounded-lg',
+                                   'class' => 'rounded-lg text-left',
                                    'target' => $button1Target,
                                ])
                                 @if (($button2Text) && ($button2Link))
@@ -113,7 +113,7 @@
                                        'href' => $button2Link,
                                        'alt' => $button2Text,
                                        'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
-                                       'class' => 'rounded-lg',
+                                       'class' => 'rounded-lg text-left',
                                        'target' => $button2Target,
                                    ])
                                 @endif
