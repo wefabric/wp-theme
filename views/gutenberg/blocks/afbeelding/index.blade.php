72b6dd9a19531eba311a9eae20734d95f30927c1
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-03-29 14:27:58 +0100

    Added padding options to image block

--- views/gutenberg/blocks/afbeelding/index.blade.php
+++ views/gutenberg/blocks/afbeelding/index.blade.php
@@ -8,6 +8,7 @@
     $overlayColor = $block['data']['overlay_color'] ?? '';
     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
 
+
     // Blokinstellingen
     $blockWidth = $block['data']['block_width'] ?? 100;
     $blockClassMap = [33 => 'w-full lg:w-1/3', 50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];
@@ -22,12 +23,43 @@
 
     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
 
+
     // Theme settings
     $options = get_fields('option');
     $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength'] ?? '' : 'rounded-none';
+
+
+    // Paddings & margins
+    $randomNumber = rand(0, 1000);
+
+    $mobilePaddingTop = $block['data']['padding_mobile_padding_top'] ?? '';
+    $mobilePaddingRight = $block['data']['padding_mobile_padding_right'] ?? '';
+    $mobilePaddingBottom = $block['data']['padding_mobile_padding_bottom'] ?? '';
+    $mobilePaddingLeft = $block['data']['padding_mobile_padding_left'] ?? '';
+    $tabletPaddingTop = $block['data']['padding_tablet_padding_top'] ?? '';
+    $tabletPaddingRight = $block['data']['padding_tablet_padding_right'] ?? '';
+    $tabletPaddingBottom = $block['data']['padding_tablet_padding_bottom'] ?? '';
+    $tabletPaddingLeft = $block['data']['padding_tablet_padding_left'] ?? '';
+    $desktopPaddingTop = $block['data']['padding_desktop_padding_top'] ?? '';
+    $desktopPaddingRight = $block['data']['padding_desktop_padding_right'] ?? '';
+    $desktopPaddingBottom = $block['data']['padding_desktop_padding_bottom'] ?? '';
+    $desktopPaddingLeft = $block['data']['padding_desktop_padding_left'] ?? '';
+
+    $mobileMarginTop = $block['data']['margin_mobile_margin_top'] ?? '';
+    $mobileMarginRight = $block['data']['margin_mobile_margin_right'] ?? '';
+    $mobileMarginBottom = $block['data']['margin_mobile_margin_bottom'] ?? '';
+    $mobileMarginLeft = $block['data']['margin_mobile_margin_left'] ?? '';
+    $tabletMarginTop = $block['data']['margin_tablet_margin_top'] ?? '';
+    $tabletMarginRight = $block['data']['margin_tablet_margin_right'] ?? '';
+    $tabletMarginBottom = $block['data']['margin_tablet_margin_bottom'] ?? '';
+    $tabletMarginLeft = $block['data']['margin_tablet_margin_left'] ?? '';
+    $desktopMarginTop = $block['data']['margin_desktop_margin_top'] ?? '';
+    $desktopMarginRight = $block['data']['margin_desktop_margin_right'] ?? '';
+    $desktopMarginBottom = $block['data']['margin_desktop_margin_bottom'] ?? '';
+    $desktopMarginLeft = $block['data']['margin_desktop_margin_left'] ?? '';
 @endphp
 
-<section id="afbeelding" class="block-afbeelding relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
+<section id="afbeelding" class="block-afbeelding afbeelding-{{ $randomNumber }}-custom-padding afbeelding-{{ $randomNumber }}-custom-margin relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
          style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">
     @if ($backgroundOverlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $backgroundOverlayColor }} opacity-{{ $backgroundOverlayOpacity }}"></div>
@@ -49,3 +81,51 @@
         </div>
     </div>
 </section>
+
+<style>
+    .afbeelding-{{ $randomNumber }}-custom-padding {
+    @media only screen and (min-width: 0px) {
+        padding-top: {{ $mobilePaddingTop }}px;
+        padding-right: {{ $mobilePaddingRight }}px;
+        padding-bottom: {{ $mobilePaddingBottom }}px;
+        padding-left: {{ $mobilePaddingLeft }}px;
+    }
+
+    @media only screen and (min-width: 768px) {
+        padding-top: {{ $tabletPaddingTop }}px;
+        padding-right: {{ $tabletPaddingRight }}px;
+        padding-bottom: {{ $tabletPaddingBottom }}px;
+        padding-left: {{ $tabletPaddingLeft }}px;
+    }
+
+    @media only screen and (min-width: 1024px) {
+        padding-top: {{ $desktopPaddingTop }}px;
+        padding-right: {{ $desktopPaddingRight }}px;
+        padding-bottom: {{ $desktopPaddingBottom }}px;
+        padding-left: {{ $desktopPaddingLeft }}px;
+    }
+    }
+
+    .afbeelding-{{ $randomNumber }}-custom-margin {
+    @media only screen and (min-width: 0px) {
+        margin-top: {{ $mobileMarginTop }}px;
+        margin-right: {{ $mobileMarginRight }}px;
+        margin-bottom: {{ $mobileMarginBottom }}px;
+        margin-left: {{ $mobileMarginLeft }}px;
+    }
+
+    @media only screen and (min-width: 768px) {
+        margin-top: {{ $tabletMarginTop }}px;
+        margin-right: {{ $tabletMarginRight }}px;
+        margin-bottom: {{ $tabletMarginBottom }}px;
+        margin-left: {{ $tabletMarginLeft }}px;
+    }
+
+    @media only screen and (min-width: 1024px) {
+        margin-top: {{ $desktopMarginTop }}px;
+        margin-right: {{ $desktopMarginRight }}px;
+        margin-bottom: {{ $desktopMarginBottom }}px;
+        margin-left: {{ $desktopMarginLeft }}px;
+    }
+    }
+</style>
