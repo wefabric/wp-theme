214b6896944860b0e49300767ff682a1951abb3d
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-04-04 09:58:28 +0200

    Updated kaarten blok

--- views/gutenberg/blocks/kaartenblok/index.blade.php
+++ views/gutenberg/blocks/kaartenblok/index.blade.php
@@ -9,15 +9,15 @@
    $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
    $titleClass = $titleClassMap[$titlePosition] ?? '';
 
-   // Buttons
-   $button1Text = $block['data']['button_bottom_button_1']['title'] ?? '';
-   $button1Link = $block['data']['button_bottom_button_1']['url'] ?? '';
-   $button1Target = $block['data']['button_bottom_button_1']['target'] ?? '_self';
-   $button1Color = $block['data']['button_bottom_button_1_color'] ?? '';
-   $button1Style = $block['data']['button_bottom_button_1_style'] ?? '';
-   $buttonCardText = $block['data']['card_button_button_text'] ?? '';
-   $buttonCardColor = $block['data']['card_button_button_color'] ?? '';
-   $buttonCardStyle = $block['data']['card_button_button_style'] ?? '';
+       // Buttons
+       $button1Text = $block['data']['button_bottom_button_1']['title'] ?? '';
+       $button1Link = $block['data']['button_bottom_button_1']['url'] ?? '';
+       $button1Target = $block['data']['button_bottom_button_1']['target'] ?? '_self';
+       $button1Color = $block['data']['button_bottom_button_1_color'] ?? '';
+       $button1Style = $block['data']['button_bottom_button_1_style'] ?? '';
+       $buttonCardText = $block['data']['card_button_button_text'] ?? '';
+       $buttonCardColor = $block['data']['card_button_button_color'] ?? '';
+       $buttonCardStyle = $block['data']['card_button_button_style'] ?? '';
 
    $cardBackgroundColor = $block['data']['card_background_color'] ?? '';
    $cardTitleColor = $block['data']['card_title_color'] ?? '';
@@ -61,6 +61,7 @@
         }
     }
 
+
    // Blokinstellingen
    $blockWidth = $block['data']['block_width'] ?? 100;
    $blockClassMap = [50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];
@@ -78,9 +79,39 @@
    // Theme settings
    $options = get_fields('option');
    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength'] ?? '' : 'rounded-none';
+
+
+   // Paddings & margins
+   $randomNumber = rand(0, 1000);
+
+   $mobilePaddingTop = $block['data']['padding_mobile_padding_top'] ?? '';
+   $mobilePaddingRight = $block['data']['padding_mobile_padding_right'] ?? '';
+   $mobilePaddingBottom = $block['data']['padding_mobile_padding_bottom'] ?? '';
+   $mobilePaddingLeft = $block['data']['padding_mobile_padding_left'] ?? '';
+   $tabletPaddingTop = $block['data']['padding_tablet_padding_top'] ?? '';
+   $tabletPaddingRight = $block['data']['padding_tablet_padding_right'] ?? '';
+   $tabletPaddingBottom = $block['data']['padding_tablet_padding_bottom'] ?? '';
+   $tabletPaddingLeft = $block['data']['padding_tablet_padding_left'] ?? '';
+   $desktopPaddingTop = $block['data']['padding_desktop_padding_top'] ?? '';
+   $desktopPaddingRight = $block['data']['padding_desktop_padding_right'] ?? '';
+   $desktopPaddingBottom = $block['data']['padding_desktop_padding_bottom'] ?? '';
+   $desktopPaddingLeft = $block['data']['padding_desktop_padding_left'] ?? '';
+
+   $mobileMarginTop = $block['data']['margin_mobile_margin_top'] ?? '';
+   $mobileMarginRight = $block['data']['margin_mobile_margin_right'] ?? '';
+   $mobileMarginBottom = $block['data']['margin_mobile_margin_bottom'] ?? '';
+   $mobileMarginLeft = $block['data']['margin_mobile_margin_left'] ?? '';
+   $tabletMarginTop = $block['data']['margin_tablet_margin_top'] ?? '';
+   $tabletMarginRight = $block['data']['margin_tablet_margin_right'] ?? '';
+   $tabletMarginBottom = $block['data']['margin_tablet_margin_bottom'] ?? '';
+   $tabletMarginLeft = $block['data']['margin_tablet_margin_left'] ?? '';
+   $desktopMarginTop = $block['data']['margin_desktop_margin_top'] ?? '';
+   $desktopMarginRight = $block['data']['margin_desktop_margin_right'] ?? '';
+   $desktopMarginBottom = $block['data']['margin_desktop_margin_bottom'] ?? '';
+   $desktopMarginLeft = $block['data']['margin_desktop_margin_left'] ?? '';
 @endphp
 
-<section id="kaarten" class="block-kaarten relative bg-{{ $backgroundColor }} {{ $customBlockClasses }} @if ($cardVariant == 'variant1') content-in-card @elseif ($cardVariant == 'variant2') content-under-card @endif"
+<section id="kaarten" class="block-kaarten kaarten-{{ $randomNumber }}-custom-padding kaarten-{{ $randomNumber }}-custom-margin relative bg-{{ $backgroundColor }} {{ $customBlockClasses }} @if ($cardVariant == 'variant1') content-in-card @elseif ($cardVariant == 'variant2') content-under-card @endif"
          style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
@@ -106,3 +137,47 @@
         </div>
     </div>
 </section>
+
+<style>
+    .kaarten-{{ $randomNumber }}-custom-padding {
+    @media only screen and (min-width: 0px) {
+        @if($mobilePaddingTop) padding-top: {{ $mobilePaddingTop }}px; @endif
+            @if($mobilePaddingRight) padding-right: {{ $mobilePaddingRight }}px; @endif
+            @if($mobilePaddingBottom) padding-bottom: {{ $mobilePaddingBottom }}px; @endif
+            @if($mobilePaddingLeft) padding-left: {{ $mobilePaddingLeft }}px; @endif
+        }
+    @media only screen and (min-width: 768px) {
+        @if($tabletPaddingTop) padding-top: {{ $tabletPaddingTop }}px; @endif
+            @if($tabletPaddingRight) padding-right: {{ $tabletPaddingRight }}px; @endif
+            @if($tabletPaddingBottom) padding-bottom: {{ $tabletPaddingBottom }}px; @endif
+            @if($tabletPaddingLeft) padding-left: {{ $tabletPaddingLeft }}px; @endif
+        }
+    @media only screen and (min-width: 1024px) {
+        @if($desktopPaddingTop) padding-top: {{ $desktopPaddingTop }}px; @endif
+            @if($desktopPaddingRight) padding-right: {{ $desktopPaddingRight }}px; @endif
+            @if($desktopPaddingBottom) padding-bottom: {{ $desktopPaddingBottom }}px; @endif
+            @if($desktopPaddingLeft) padding-left: {{ $desktopPaddingLeft }}px; @endif
+        }
+    }
+
+    .kaarten-{{ $randomNumber }}-custom-margin {
+    @media only screen and (min-width: 0px) {
+        @if($mobileMarginTop) margin-top: {{ $mobileMarginTop }}px; @endif
+            @if($mobileMarginRight) margin-right: {{ $mobileMarginRight }}px; @endif
+            @if($mobileMarginBottom) margin-bottom: {{ $mobileMarginBottom }}px; @endif
+            @if($mobileMarginLeft) margin-left: {{ $mobileMarginLeft }}px; @endif
+        }
+    @media only screen and (min-width: 768px) {
+        @if($tabletMarginTop) margin-top: {{ $tabletMarginTop }}px; @endif
+            @if($tabletMarginRight) margin-right: {{ $tabletMarginRight }}px; @endif
+            @if($tabletMarginBottom) margin-bottom: {{ $tabletMarginBottom }}px; @endif
+            @if($tabletMarginLeft) margin-left: {{ $tabletMarginLeft }}px; @endif
+        }
+    @media only screen and (min-width: 1024px) {
+        @if($desktopMarginTop) margin-top: {{ $desktopMarginTop }}px; @endif
+            @if($desktopMarginRight) margin-right: {{ $desktopMarginRight }}px; @endif
+            @if($desktopMarginBottom) margin-bottom: {{ $desktopMarginBottom }}px; @endif
+            @if($desktopMarginLeft) margin-left: {{ $desktopMarginLeft }}px; @endif
+        }
+    }
+</style>
