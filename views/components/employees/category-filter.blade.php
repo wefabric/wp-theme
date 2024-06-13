38ae719b72488c5c773556d9f537b526c068060e
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-06-13 12:40:25 +0200

    Added filter for employees

---
+++ views/components/employees/category-filter.blade.php
@@ -0,0 +1,53 @@
+@php
+    // Get current category IDs from URL query
+    $currentCatIds = isset($_GET['employee_category']) ? array_map('intval', explode(',', $_GET['employee_category'])) : [];
+    $currentTerms = [];
+    foreach ($currentCatIds as $catId) {
+        $currentTerms[] = get_term($catId, 'employee_categories');
+    }
+
+    $categories = get_terms([
+        'taxonomy' => 'employee_categories',
+        'hide_empty' => true,
+    ]);
+@endphp
+
+<div class="category-list block flex flex-wrap gap-2">
+    @if($categories && !is_wp_error($categories))
+        @foreach($categories as $category)
+            @php
+                $categoryColor = get_field('category_color', 'employee_categories_' . $category->term_id);
+                $isActive = in_array($category->term_id, $currentCatIds);
+                $newCatIds = $isActive ? array_diff($currentCatIds, [$category->term_id]) : array_merge($currentCatIds, [$category->term_id]);
+                $url = empty($newCatIds) ? remove_query_arg('employee_category') : add_query_arg('employee_category', implode(',', $newCatIds), get_permalink());
+                $style = '';
+                $class = 'category hover:text-white border-2 category-link px-4 py-2 rounded-full';
+
+                if ($isActive && $categoryColor) {
+                    // Active category with color
+                    $style = "border: 2px solid $categoryColor; background-color: $categoryColor; color: white;";
+                    $class .= " bg-primary text-white";
+                } elseif ($isActive && !$categoryColor) {
+                    // Active category without color
+                    $class .= " border-primary bg-primary text-white";
+                } elseif (!$isActive && $categoryColor) {
+                    // Inactive category with color
+                    $style = "border: 2px solid $categoryColor; color: $categoryColor;";
+                    $class .= " border-primary hover:bg-primary hover:text-white";
+                } else {
+                    // Inactive category without color
+                    $class .= " border-primary hover:bg-primary hover:text-white";
+                }
+            @endphp
+            <a href="{{ $url }}"
+               style="{{ $style }}"
+               class="{{ $class }}"
+               @if(!$isActive && $categoryColor)
+                   onmouseover="this.style.backgroundColor='{{ $categoryColor }}'; this.style.color='white';"
+                   onmouseout="this.style.backgroundColor=''; this.style.color='{{ $categoryColor }}';"
+               @endif>
+               {!! $category->name !!}
+            </a>
+        @endforeach
+    @endif
+</div>
--- views/gutenberg/blocks/werknemers/index.blade.php
+++ views/gutenberg/blocks/werknemers/index.blade.php
@@ -1,75 +1,127 @@
 @php
-    // Content
-    $title = $block['data']['title'] ?? '';
-    $titleColor = $block['data']['title_color'] ?? '';
-    $titlePosition = $block['data']['title_position'] ?? '';
-    $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
-    $titleClass = $titleClassMap[$titlePosition] ?? '';
+     // Content
+     $title = $block['data']['title'] ?? '';
+     $titleColor = $block['data']['title_color'] ?? '';
+     $titlePosition = $block['data']['title_position'] ?? '';
+     $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
+     $titleClass = $titleClassMap[$titlePosition] ?? '';
 
-    $employeeTitleColor = $block['data']['employee_title_color'] ?? '';
-    $employeeTextColor = $block['data']['employee_text_color'] ?? '';
+     $employeeTitleColor = $block['data']['employee_title_color'] ?? '';
+     $employeeTextColor = $block['data']['employee_text_color'] ?? '';
 
-    // Buttons
-    $button1Text = $block['data']['button_button_1']['title'] ?? '';
-    $button1Link = $block['data']['button_button_1']['url'] ?? '';
-    $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
-    $button1Color = $block['data']['button_button_1_color'] ?? '';
-    $button1Style = $block['data']['button_button_1_style'] ?? '';
+     // Buttons
+     $button1Text = $block['data']['button_button_1']['title'] ?? '';
+     $button1Link = $block['data']['button_button_1']['url'] ?? '';
+     $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
+     $button1Color = $block['data']['button_button_1_color'] ?? '';
+     $button1Style = $block['data']['button_button_1_style'] ?? '';
 
-    // Show employees
-    $displayType = $block['data']['display_type'];
+     // Show employees
+     $displayType = $block['data']['display_type'];
+     $currentTerms = isset($_GET['employee_category']) ? array_map('intval', explode(',', $_GET['employee_category'])) : [];
 
-    if ($displayType == 'show_all') {
-        $args = [
-            'posts_per_page' => -1,
-            'post_type' => 'werknemers',
-            'post_status' => 'publish',
-        ];
+     // Show all
+     if ($displayType == 'show_all') {
+          $args = [
+             'posts_per_page' => -1,
+             'post_type' => 'werknemers',
+             'post_status' => 'publish',
+         ];
 
-        $query = new WP_Query($args);
-        $employees = wp_list_pluck($query->posts, 'ID');
-    }
-    elseif ($displayType == 'show_category') {
-        $selectedCategory = $block['data']['category'] ?? '';
-        $args = [
-            'posts_per_page' => -1,
-            'post_type' => 'werknemers',
-            'post_status' => 'publish',
-            'tax_query' => [
-                [
-                    'taxonomy' => 'employee_categories',
-                    'field' => 'id',
-                    'terms' => $selectedCategory,
-                ],
-            ],
-        ];
-        $query = new WP_Query($args);
-        $employees = wp_list_pluck($query->posts, 'ID');
-    }
+         if ($currentTerms) {
+             $args['tax_query'] = [
+                 [
+                     'taxonomy' => 'employee_categories',
+                     'field' => 'id',
+                     'terms' => $currentTerms,
+                 ],
+             ];
+         }
+
+         $query = new WP_Query($args);
+         $employees = wp_list_pluck($query->posts, 'ID');
+     }
+
+     // Show category
+     elseif ($displayType == 'show_category') {
+         $selectedCategory = $block['data']['category'] ?? '';
+
+         $args = [
+             'posts_per_page' => -1,
+             'post_type' => 'werknemers',
+             'post_status' => 'publish',
+             'tax_query' => [
+                 [
+                     'taxonomy' => 'employee_categories',
+                     'field' => 'id',
+                     'terms' => $selectedCategory,
+                 ],
+             ],
+         ];
+
+         if ($currentTerms) {
+             $args['tax_query'][] = [
+                 'taxonomy' => 'employee_categories',
+                 'field' => 'id',
+                 'terms' => $currentTerms,
+             ];
+             $args['tax_query']['relation'] = 'AND';
+         }
+
+         $query = new WP_Query($args);
+         $employees = wp_list_pluck($query->posts, 'ID');
+     }
+
+    // Show specific
     elseif ($displayType == 'show_specific') {
-        $employees = $block['data']['show_specific_employees'];
-            if (!is_array($employees) || empty($employees)) {
-                $employees = [];
-            }
-    }
+         $employees = $block['data']['show_specific_employees'];
+         if (!is_array($employees) || empty($employees)) {
+             $employees = [];
+         }
 
-    // Blokinstellingen
-    $blockWidth = $block['data']['block_width'] ?? 100;
-    $blockClassMap = [50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];
-    $blockClass = $blockClassMap[$blockWidth] ?? '';
-    $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';
+         $args = [
+             'posts_per_page' => -1,
+             'post_type' => 'werknemers',
+             'post_status' => 'publish',
+             'tax_query' => [],
+         ];
 
-    $backgroundColor = $block['data']['background_color'] ?? 'none';
-    $imageId = $block['data']['background_image'] ?? '';
-    $overlayEnabled = $block['data']['overlay_image'] ?? false;
-    $overlayColor = $block['data']['overlay_color'] ?? '';
-    $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
+         if ($currentTerms) {
+             $args['tax_query'][] = [
+                 'taxonomy' => 'employee_categories',
+                 'field' => 'id',
+                 'terms' => $currentTerms,
+             ];
+             $args['tax_query']['relation'] = 'AND';
+         }
 
-    $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
+         if (!empty($employees)) {
+             $args['post__in'] = $employees;
+         }
 
-    // Theme settings
-    $options = get_fields('option');
-    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength']??'': 'rounded-none';
+         $query = new WP_Query($args);
+         $employees = wp_list_pluck($query->posts, 'ID');
+     }
+
+     $visibleElements = $block['data']['show_element'] ?? [];
+
+     // Blokinstellingen
+     $blockWidth = $block['data']['block_width'] ?? 100;
+     $blockClassMap = [50 => 'w-full lg:w-1/2', 66 => 'w-full lg:w-2/3', 80 => 'w-full lg:w-4/5', 100 => 'w-full', 'fullscreen' => 'w-full'];
+     $blockClass = $blockClassMap[$blockWidth] ?? '';
+     $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';
+
+     $backgroundColor = $block['data']['background_color'] ?? 'none';
+     $imageId = $block['data']['background_image'] ?? '';
+     $overlayEnabled = $block['data']['overlay_image'] ?? false;
+     $overlayColor = $block['data']['overlay_color'] ?? '';
+     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
+
+     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
+
+     // Theme settings
+     $options = get_fields('option');
+     $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength']??'': 'rounded-none';
 @endphp
 
 <section id="werknemers" class="block-werknemers relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
@@ -82,21 +134,24 @@
             @if ($title)
                 <h2 class="text-{{ $titleColor }} container mx-auto lg:mb-4 @if($blockWidth == 'fullscreen') px-8 @endif {{ $titleClass }}">{!! $title !!}</h2>
             @endif
+            @if (!empty($visibleElements) && in_array('category_filter', $visibleElements))
+               @include('components.employees.category-filter')
+            @endif
             @if ($employees)
-                @include('components.employees.list', ['employees' => $employees])
+               @include('components.employees.list', ['employees' => $employees])
             @endif
             @if (($button1Text) && ($button1Link))
                 <div class="bottom-button w-full text-center mt-4 md:mt-8">
-                    @include('components.buttons.default', [
-                       'text' => $button1Text,
-                       'href' => $button1Link,
-                       'alt' => $button1Text,
-                       'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                       'class' => 'rounded-lg text-left',
-                       'target' => $button1Target,
-                   ])
-                </div>
-            @endif
+                   @include('components.buttons.default', [
+                      'text' => $button1Text,
+                      'href' => $button1Link,
+                      'alt' => $button1Text,
+                      'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
+                      'class' => 'rounded-lg text-left',
+                      'target' => $button1Target,
+                  ])
+               </div>
+           @endif
         </div>
     </div>
 </section>
