06c89d523bb9523f8d48030dcf1069dd0121d870
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-06-27 16:36:16 +0200

    Update index.blade.php

--- views/gutenberg/blocks/foto-slider/index.blade.php
+++ views/gutenberg/blocks/foto-slider/index.blade.php
@@ -79,7 +79,7 @@
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
-    <div class="relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $containerClass }}">
+    <div class="custom-styling relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $containerClass }}">
         <div class="{{ $blockClass }} mx-auto">
             @if ($title)
                 <h2 class="lg:mb-4 text-{{ $titleColor }}  @if($blockWidth == 'fullscreen') px-8 @endif {{ $titleClass }}">{!! $title !!}</h2>
