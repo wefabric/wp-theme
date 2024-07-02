53a74cfbebc6cb52ee5c8611eb6da806f753c9d1
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-07-02 15:55:02 +0200

    Updated USP block

--- views/components/usps/list-item.blade.php
+++ views/components/usps/list-item.blade.php
@@ -1,16 +1,22 @@
 @php
-    $title = $usp['uspTitle'];
-    $text = $usp['uspText'];
+    $uspTitle = $usp['uspTitle'];
+    $uspText = $usp['uspText'];
     $icon = $usp['uspIcon'];
     $iconColor = $usp['uspIconColor'];
     $imageID = $usp['uspImage'];
     $uspTitleColor = $block['data']['usp_title_color'] ?? '';
     $uspTextColor = $block['data']['usp_text_color'] ?? '';
     $altText = get_post_meta($imageID, '_wp_attachment_image_alt', true) ?: 'usp-image';
+    $numberAnimation = $block['data']['number_animation'] ?? false;
+
+    // Extract numeric part from $uspTitle
+    preg_match('/(\d+)/', $uspTitle, $matches);
+    $numericPart = $matches[0] ?? '';
+    $isNumber = !empty($numericPart);
 @endphp
 
 <div class="USP-item h-full usp-{{ $uspLayout }} ">
-    <div class="item-styling h-full @if( $uspLayout == 'horizontal') flex flex-row gap-x-6 items-center text-left @elseif( $uspLayout == 'vertical') flex flex-col gap-y-4 text-center @endif">
+    <div class="item-styling h-full @if( $uspLayout == 'horizontal') flex flex-row gap-x-6 items-center text-left justify-center @elseif( $uspLayout == 'vertical') flex flex-col gap-y-4 text-center @endif">
         @if ($icon)
             <i class="fa-{{ $icon['style'] }} fa-{{ $icon['id'] }} text-{{ $iconColor }} text-[70px] inline-block"
                aria-hidden="true"></i>
@@ -24,15 +30,69 @@
                 'alt' => $altText,
             ])
         @endif
-        @if ($title || $text)
+        @if ($uspTitle || $uspText)
             <div class="usp-data">
-                @if ($title)
-                    <p class="usp-title text-{{ $uspTitleColor }} font-bold h4">{!! $title !!}</p>
+                @if ($uspTitle)
+                    <p class="usp-title text-{{ $uspTitleColor }} font-bold h4">
+                        @if ($isNumber && $numberAnimation)
+                            <span class="counter" data-target="{{ $numericPart }}">0</span>{{ str_replace($numericPart, '', $uspTitle) }}
+                        @else
+                            {!! $uspTitle !!}
+                        @endif
+                    </p>
                 @endif
-                @if ($text)
-                    @include('components.content', ['content' => apply_filters('the_content', $text), 'class' => 'usp-text mt-4 text-' . $uspTextColor])
+                @if ($uspText)
+                    @include('components.content', ['content' => apply_filters('the_content', $uspText), 'class' => 'usp-text mt-2 text-' . $uspTextColor])
                 @endif
             </div>
         @endif
     </div>
 </div>
+
+@if ($isNumber && $numberAnimation)
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const counters = document.querySelectorAll('.counter');
+            const observerOptions = {
+                root: null, // Use the viewport as the root
+                rootMargin: '0px',
+                threshold: 0.1 // Trigger when at least 10% of the element is in the viewport
+            };
+
+            const observerCallback = (entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const counter = entry.target;
+                        const target = +counter.getAttribute('data-target');
+                        const duration = 1000; // Total duration in milliseconds (5 seconds)
+                        const frameRate = 60; // 60 updates per second
+                        const updateInterval = 1000 / frameRate; // Interval between updates in milliseconds
+                        const totalSteps = duration / updateInterval; // Total number of steps
+                        const increment = target / totalSteps; // Increment per step
+
+                        let count = 0;
+
+                        const updateCounter = () => {
+                            count += increment;
+                            if (count < target) {
+                                counter.innerText = Math.ceil(count);
+                                setTimeout(updateCounter, updateInterval);
+                            } else {
+                                counter.innerText = target;
+                            }
+                        };
+
+                        updateCounter();
+                        observer.unobserve(counter); // Stop observing once the animation starts
+                    }
+                });
+            };
+
+            const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+            counters.forEach(counter => {
+                observer.observe(counter);
+            });
+        });
+    </script>
+@endif
--- views/gutenberg/blocks/usp/index.blade.php
+++ views/gutenberg/blocks/usp/index.blade.php
@@ -1,31 +1,32 @@
 @php
     // Content
-    $title = $block['data']['title'];
+    $title = $block['data']['title'] ?? '';
+    $subTitle = $block['data']['subtitle'] ?? '';
     $titleColor = $block['data']['title_color'] ?? '';
-    $titlePosition = $block['data']['title_position'] ?? '';
-    $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
-    $titleClass = $titleClassMap[$titlePosition] ?? '';
+    $text = $block['data']['text'] ?? '';
     $textColor = $block['data']['text_color'] ?? '';
 
     $uspLayout = $block['data']['usp_layout'] ?? 'vertical';
 
-    // Buttons
-    $button1Text = $block['data']['button_button_1']['title'] ?? '';
-    $button1Link = $block['data']['button_button_1']['url'] ?? '';
-    $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
-    $button1Color = $block['data']['button_button_1_color'] ?? '';
-    $button1Style = $block['data']['button_button_1_style'] ?? '';
+   // Buttons
+        $button1Text = $block['data']['button_button_1']['title'] ?? '';
+        $button1Link = $block['data']['button_button_1']['url'] ?? '';
+        $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
+        $button1Color = $block['data']['button_button_1_color'] ?? '';
+        $button1Style = $block['data']['button_button_1_style'] ?? '';
 
-   // Show usps
+        $textPosition = $block['data']['text_position'] ?? '';
+        $textClassMap = ['left' => 'text-left justify-start', 'center' => 'text-center justify-center', 'right' => 'text-right justify-end',];
+        $textClass = $textClassMap[$textPosition] ?? '';
+
+    // Show usps
     $uspsCount = $block['data']['usps'];
     $usps = [];
 
     for ($i = 0; $i < $uspsCount; $i++) {
         $uspTitle = $block['data']["usps_{$i}_usp_title"];
         $uspText = $block['data']["usps_{$i}_usp_text"];
-
         $uspImage = $block['data']["usps_{$i}_image"] ?? '';
-
         $uspIcon = $block['data']["usps_{$i}_icon"] ?? null;
         $uspIconColor = $block['data']["usps_{$i}_icon_color"] ?? '';
 
@@ -50,12 +51,13 @@
     $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';
 
     $backgroundColor = $block['data']['background_color'] ?? 'none';
-    $imageId = $block['data']['background_image'] ?? '';
+    $backgroundImageId = $block['data']['background_image'] ?? '';
     $overlayEnabled = $block['data']['overlay_image'] ?? false;
     $overlayColor = $block['data']['overlay_color'] ?? '';
     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
 
     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
+    $hideBlock = $block['data']['hide_block'] ?? false;
 
 
     // Paddings & margins
@@ -88,49 +90,58 @@
     $desktopMarginLeft = $block['data']['margin_desktop_margin_left'] ?? '';
 @endphp
 
-<section id="usps" class="block-usps relative usp-{{ $randomNumber }}-custom-padding usp-{{ $randomNumber }}-custom-margin layout-{{ $uspLayout }} bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
-         style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">
+<section id="usps" class="block-usps relative usp-{{ $randomNumber }}-custom-padding usp-{{ $randomNumber }}-custom-margin layout-{{ $uspLayout }} bg-{{ $backgroundColor }} {{ $customBlockClasses }} {{ $hideBlock ? 'hidden' : '' }}"
+         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
-    <div class="layout relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
-        <div class="custom-styling {{ $blockClass }} mx-auto relative">
-            @if ($title)
-                <h2 class="container mx-auto lg:mb-8 @if($blockWidth == 'fullscreen') px-8 @endif {{ $titleClass }} text-{{ $titleColor }}">{!! $title !!}</h2>
-            @endif
-            @include('components.usps.list', ['usps' => $usps])
-            @if (($button1Text) && ($button1Link))
-                <div class="bottom-button w-full text-center mt-4 md:mt-8">
-                    @include('components.buttons.default', [
-                       'text' => $button1Text,
-                       'href' => $button1Link,
-                       'alt' => $button1Text,
-                       'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                       'class' => 'rounded-lg text-left',
-                       'target' => $button1Target,
-                   ])
-                </div>
-            @endif
+        <div class="custom-styling relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
+            <div class="{{ $blockClass }} {{ $textClass }} mx-auto">
+                @if ($subTitle)
+                    <span class="subtitle block mb-2 text-{{ $titleColor }} container mx-auto @if($blockWidth == 'fullscreen') px-8 @endif">{!! $subTitle !!}</span>
+                @endif
+                @if ($title)
+                    <h2 class="title mb-4 text-{{ $titleColor }} container mx-auto @if($blockWidth == 'fullscreen') px-8 @endif">{!! $title !!}</h2>
+                @endif
+                @if ($text)
+                    @include('components.content', [
+                        'content' => apply_filters('the_content', $text),
+                        'class' => 'container mx-auto mb-4 text-' . $textColor . ($blockWidth == 'fullscreen' ? ' px-8' : '')
+                    ])
+                @endif
+                @include('components.usps.list', ['usps' => $usps])
+                @if (($button1Text) && ($button1Link))
+                    <div class="{{ $textClass }} bottom-button w-full flex sm:flex-row gap-4 mt-4 md:mt-8 container mx-auto @if($blockWidth == 'fullscreen') px-8 @endif">
+                        @include('components.buttons.default', [
+                           'text' => $button1Text,
+                           'href' => $button1Link,
+                           'alt' => $button1Text,
+                           'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
+                           'class' => 'rounded-lg text-left',
+                           'target' => $button1Target,
+                       ])
+                    </div>
+                @endif
+            </div>
         </div>
-    </div>
 </section>
 
 <style>
     .usp-{{ $randomNumber }}-custom-padding {
-    @media only screen and (min-width: 0px) {
-        @if($mobilePaddingTop) padding-top: {{ $mobilePaddingTop }}px; @endif
+        @media only screen and (min-width: 0px) {
+            @if($mobilePaddingTop) padding-top: {{ $mobilePaddingTop }}px; @endif
             @if($mobilePaddingRight) padding-right: {{ $mobilePaddingRight }}px; @endif
             @if($mobilePaddingBottom) padding-bottom: {{ $mobilePaddingBottom }}px; @endif
             @if($mobilePaddingLeft) padding-left: {{ $mobilePaddingLeft }}px; @endif
         }
-    @media only screen and (min-width: 768px) {
-        @if($tabletPaddingTop) padding-top: {{ $tabletPaddingTop }}px; @endif
+        @media only screen and (min-width: 768px) {
+            @if($tabletPaddingTop) padding-top: {{ $tabletPaddingTop }}px; @endif
             @if($tabletPaddingRight) padding-right: {{ $tabletPaddingRight }}px; @endif
-            @if($tabletPaddingBottom) padding-bottom: {{ $tabletPaddingBottom }}px; @endif
-            @if($tabletPaddingLeft) padding-left: {{ $tabletPaddingLeft }}px; @endif
+                @if($tabletPaddingBottom) padding-bottom: {{ $tabletPaddingBottom }}px; @endif
+                @if($tabletPaddingLeft) padding-left: {{ $tabletPaddingLeft }}px; @endif
         }
-    @media only screen and (min-width: 1024px) {
-        @if($desktopPaddingTop) padding-top: {{ $desktopPaddingTop }}px; @endif
+        @media only screen and (min-width: 1024px) {
+            @if($desktopPaddingTop) padding-top: {{ $desktopPaddingTop }}px; @endif
             @if($desktopPaddingRight) padding-right: {{ $desktopPaddingRight }}px; @endif
             @if($desktopPaddingBottom) padding-bottom: {{ $desktopPaddingBottom }}px; @endif
             @if($desktopPaddingLeft) padding-left: {{ $desktopPaddingLeft }}px; @endif
@@ -138,20 +149,20 @@
     }
 
     .usp-{{ $randomNumber }}-custom-margin {
-    @media only screen and (min-width: 0px) {
-        @if($mobileMarginTop) margin-top: {{ $mobileMarginTop }}px; @endif
+        @media only screen and (min-width: 0px) {
+            @if($mobileMarginTop) margin-top: {{ $mobileMarginTop }}px; @endif
             @if($mobileMarginRight) margin-right: {{ $mobileMarginRight }}px; @endif
             @if($mobileMarginBottom) margin-bottom: {{ $mobileMarginBottom }}px; @endif
             @if($mobileMarginLeft) margin-left: {{ $mobileMarginLeft }}px; @endif
         }
-    @media only screen and (min-width: 768px) {
-        @if($tabletMarginTop) margin-top: {{ $tabletMarginTop }}px; @endif
+        @media only screen and (min-width: 768px) {
+            @if($tabletMarginTop) margin-top: {{ $tabletMarginTop }}px; @endif
             @if($tabletMarginRight) margin-right: {{ $tabletMarginRight }}px; @endif
             @if($tabletMarginBottom) margin-bottom: {{ $tabletMarginBottom }}px; @endif
             @if($tabletMarginLeft) margin-left: {{ $tabletMarginLeft }}px; @endif
         }
-    @media only screen and (min-width: 1024px) {
-        @if($desktopMarginTop) margin-top: {{ $desktopMarginTop }}px; @endif
+        @media only screen and (min-width: 1024px) {
+            @if($desktopMarginTop) margin-top: {{ $desktopMarginTop }}px; @endif
             @if($desktopMarginRight) margin-right: {{ $desktopMarginRight }}px; @endif
             @if($desktopMarginBottom) margin-bottom: {{ $desktopMarginBottom }}px; @endif
             @if($desktopMarginLeft) margin-left: {{ $desktopMarginLeft }}px; @endif
