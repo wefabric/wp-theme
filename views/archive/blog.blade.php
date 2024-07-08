d086be9cbafcd7f5b5cc0c61d5056c651a1f2891
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-07-08 10:14:03 +0200

    Update blog.blade.php

--- views/archive/blog.blade.php
+++ views/archive/blog.blade.php
@@ -13,12 +13,14 @@
 {{--	@include('components.breadcrumbs.index', ['classes' => ''])--}}
 
 	<div class="container px-8 pt-6 lg:pt-12 mx-auto">
-		@include('components.news.category-list')
+		<div class="w-full lg:w-4/5 mx-auto">
+			@include('components.news.category-list')
+		</div>
 	</div>
 
 	<section class="news-archive-grid relative">
-		<div class="container mx-auto my-12 lg:my-12">
-			<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-4 lg:gap-x-8 px-8">
+		<div class="container mx-auto my-12 lg:my-12 w-full lg:w-4/5 mx-auto">
+			<div class="w-full lg:w-4/5 mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-4 lg:gap-x-8 px-8">
 				@loop
 					@include('components.cards.newsitem', [
 						'item' => get_the_ID()
