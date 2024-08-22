5b4b3c5a46c33310fdc32311803d3db913e49ff1
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-08-22 10:27:54 +0200

    Updated FAQ block

--- views/gutenberg/blocks/faq/index.blade.php
+++ views/gutenberg/blocks/faq/index.blade.php
@@ -1,16 +1,22 @@
 @php
     // Content
     $title = $block['data']['title'] ?? '';
+    $subTitle = $block['data']['subtitle'] ?? '';
     $titleColor = $block['data']['title_color'] ?? '';
+
     $titlePosition = $block['data']['title_position'] ?? '';
     $titleClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
     $titleClass = $titleClassMap[$titlePosition] ?? '';
+
+    $questionTextColor = $block['data']['question_text_color'] ?? '';
     $textColor = $block['data']['text_color'] ?? '';
+
 
     // FAQ
     $faqId = $block['data']['faq_group'] ?? '';
     $questionsAndAnswers = get_field('faq', $faqId);
     $faqBackgroundColor = $block['data']['faq_background_color'] ?? 'default-color';
+
 
     // Blokinstellingen
     $blockWidth = $block['data']['block_width'] ?? 100;
@@ -19,32 +25,66 @@
     $fullScreenClass = $blockWidth !== 'fullscreen' ? 'container mx-auto' : '';
 
     $backgroundColor = $block['data']['background_color'] ?? 'default-color';
-    $imageId = $block['data']['background_image'] ?? '';
+    $backgroundImageId = $block['data']['background_image'] ?? '';
     $overlayEnabled = $block['data']['overlay_image'] ?? false;
     $overlayColor = $block['data']['overlay_color'] ?? '';
     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
 
     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
+    $hideBlock = $block['data']['hide_block'] ?? false;
+
+
+    // Paddings & margins
+    $randomNumber = rand(0, 1000);
+
+    $mobilePaddingTop = $block['data']['padding_mobile_padding_top'] ?? '';
+    $mobilePaddingRight = $block['data']['padding_mobile_padding_right'] ?? '';
+    $mobilePaddingBottom = $block['data']['padding_mobile_padding_bottom'] ?? '';
+    $mobilePaddingLeft = $block['data']['padding_mobile_padding_left'] ?? '';
+    $tabletPaddingTop = $block['data']['padding_tablet_padding_top'] ?? '';
+    $tabletPaddingRight = $block['data']['padding_tablet_padding_right'] ?? '';
+    $tabletPaddingBottom = $block['data']['padding_tablet_padding_bottom'] ?? '';
+    $tabletPaddingLeft = $block['data']['padding_tablet_padding_left'] ?? '';
+    $desktopPaddingTop = $block['data']['padding_desktop_padding_top'] ?? '';
+    $desktopPaddingRight = $block['data']['padding_desktop_padding_right'] ?? '';
+    $desktopPaddingBottom = $block['data']['padding_desktop_padding_bottom'] ?? '';
+    $desktopPaddingLeft = $block['data']['padding_desktop_padding_left'] ?? '';
+
+    $mobileMarginTop = $block['data']['margin_mobile_margin_top'] ?? '';
+    $mobileMarginRight = $block['data']['margin_mobile_margin_right'] ?? '';
+    $mobileMarginBottom = $block['data']['margin_mobile_margin_bottom'] ?? '';
+    $mobileMarginLeft = $block['data']['margin_mobile_margin_left'] ?? '';
+    $tabletMarginTop = $block['data']['margin_tablet_margin_top'] ?? '';
+    $tabletMarginRight = $block['data']['margin_tablet_margin_right'] ?? '';
+    $tabletMarginBottom = $block['data']['margin_tablet_margin_bottom'] ?? '';
+    $tabletMarginLeft = $block['data']['margin_tablet_margin_left'] ?? '';
+    $desktopMarginTop = $block['data']['margin_desktop_margin_top'] ?? '';
+    $desktopMarginRight = $block['data']['margin_desktop_margin_right'] ?? '';
+    $desktopMarginBottom = $block['data']['margin_desktop_margin_bottom'] ?? '';
+    $desktopMarginLeft = $block['data']['margin_desktop_margin_left'] ?? '';
 @endphp
 
-<section id="faq" class="block-faq relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
-         style="background-image: url('{{ wp_get_attachment_image_url($imageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($imageId) }}">
+<section id="faq" class="block-faq relative faq-{{ $randomNumber }}-custom-padding faq-{{ $randomNumber }}-custom-margin bg-{{ $backgroundColor }} {{ $customBlockClasses }} {{ $hideBlock ? 'hidden' : '' }}"
+         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
     <div class="relative z-10 px-8 py-8 lg:py-16 xl:py-20 {{ $fullScreenClass }}">
         <div class="mx-auto {{ $blockClass }} {{ $titleClass }}">
             <div class="faq-drawer container mx-auto @if($blockWidth == 'fullscreen') px-8 @endif">
+                @if ($subTitle)
+                    <span class="subtitle block mb-2 text-{{ $titleColor }}">{!! $subTitle !!}</span>
+                @endif
                 @if ($title)
                     <h2 class="mb-8 text-{{ $titleColor }}">{!! $title !!}</h2>
                 @endif
                 @if ($questionsAndAnswers)
                     @foreach ($questionsAndAnswers as $key => $faq)
-                        <div class="mb-2 text-left text-{{ $textColor }}">
+                        <div class="mb-2 text-left ">
                             <input class="faq-drawer__trigger mb-4" id="faq-drawer-{{$key}}" type="checkbox"/>
-                            <label class="faq-drawer__title relative block cursor-pointer text-md font-bold p-10 bg-{{ $faqBackgroundColor }}"
+                            <label class="faq-drawer__title relative block cursor-pointer text-{{ $questionTextColor }} text-md font-bold p-10 bg-{{ $faqBackgroundColor }}"
                                    for="faq-drawer-{{$key}}">{{ $faq['question_and_answer']['question'] }}</label>
-                            <div class="faq-drawer__content-wrapper">
+                            <div class="faq-drawer__content-wrapper text-{{ $textColor }}">
                                 <div class="faq-drawer__content px-10 pb-8 bg-{{ $faqBackgroundColor }}">
                                     <div class="text-base ">{!! apply_filters('the_content', $faq['question_and_answer']['answer']) !!}</div>
                                 </div>
@@ -56,3 +96,47 @@
         </div>
     </div>
 </section>
+
+<style>
+    .faq-{{ $randomNumber }}-custom-padding {
+        @media only screen and (min-width: 0px) {
+            @if($mobilePaddingTop) padding-top: {{ $mobilePaddingTop }}px; @endif
+            @if($mobilePaddingRight) padding-right: {{ $mobilePaddingRight }}px; @endif
+            @if($mobilePaddingBottom) padding-bottom: {{ $mobilePaddingBottom }}px; @endif
+            @if($mobilePaddingLeft) padding-left: {{ $mobilePaddingLeft }}px; @endif
+        }
+        @media only screen and (min-width: 768px) {
+            @if($tabletPaddingTop) padding-top: {{ $tabletPaddingTop }}px; @endif
+            @if($tabletPaddingRight) padding-right: {{ $tabletPaddingRight }}px; @endif
+            @if($tabletPaddingBottom) padding-bottom: {{ $tabletPaddingBottom }}px; @endif
+            @if($tabletPaddingLeft) padding-left: {{ $tabletPaddingLeft }}px; @endif
+        }
+        @media only screen and (min-width: 1024px) {
+            @if($desktopPaddingTop) padding-top: {{ $desktopPaddingTop }}px; @endif
+            @if($desktopPaddingRight) padding-right: {{ $desktopPaddingRight }}px; @endif
+            @if($desktopPaddingBottom) padding-bottom: {{ $desktopPaddingBottom }}px; @endif
+            @if($desktopPaddingLeft) padding-left: {{ $desktopPaddingLeft }}px; @endif
+        }
+    }
+
+    .faq-{{ $randomNumber }}-custom-margin {
+        @media only screen and (min-width: 0px) {
+            @if($mobileMarginTop) margin-top: {{ $mobileMarginTop }}px; @endif
+            @if($mobileMarginRight) margin-right: {{ $mobileMarginRight }}px; @endif
+            @if($mobileMarginBottom) margin-bottom: {{ $mobileMarginBottom }}px; @endif
+            @if($mobileMarginLeft) margin-left: {{ $mobileMarginLeft }}px; @endif
+        }
+        @media only screen and (min-width: 768px) {
+            @if($tabletMarginTop) margin-top: {{ $tabletMarginTop }}px; @endif
+            @if($tabletMarginRight) margin-right: {{ $tabletMarginRight }}px; @endif
+            @if($tabletMarginBottom) margin-bottom: {{ $tabletMarginBottom }}px; @endif
+            @if($tabletMarginLeft) margin-left: {{ $tabletMarginLeft }}px; @endif
+        }
+        @media only screen and (min-width: 1024px) {
+            @if($desktopMarginTop) margin-top: {{ $desktopMarginTop }}px; @endif
+            @if($desktopMarginRight) margin-right: {{ $desktopMarginRight }}px; @endif
+            @if($desktopMarginBottom) margin-bottom: {{ $desktopMarginBottom }}px; @endif
+            @if($desktopMarginLeft) margin-left: {{ $desktopMarginLeft }}px; @endif
+        }
+    }
+</style>
