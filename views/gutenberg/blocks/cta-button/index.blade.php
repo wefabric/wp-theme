27c45b309317c3b5a08511551ebcedc0eff1a837
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-09-26 16:34:40 +0200

    Updated CTA button block

--- views/gutenberg/blocks/cta-button/index.blade.php
+++ views/gutenberg/blocks/cta-button/index.blade.php
@@ -10,7 +10,7 @@
         $textClassMap = ['left' => 'text-left', 'center' => 'text-center', 'right' => 'text-right',];
         $textClass = $textClassMap[$textPosition] ?? 'text-left';
 
-        $flexClassMap = ['left' => 'items-start', 'center' => 'items-center', 'right' => 'items-end',];
+    $flexClassMap = ['left' => 'items-start', 'center' => 'items-center', 'right' => 'items-end',];
     $flexClass = $flexClassMap[$textPosition] ?? 'items-center';
     $ctaLayout = $block['data']['cta_layout'] ?? '';
     $flexDirection = ($ctaLayout === 'vertical') ? 'flex-col' : (($ctaLayout === 'horizontal') ? 'flex-row' : '');
@@ -31,11 +31,29 @@
     $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
     $button1Color = $block['data']['button_button_1_color'] ?? '';
     $button1Style = $block['data']['button_button_1_style'] ?? '';
+    $button1Download = $block['data']['button_button_1_download'] ?? false;
+    $button1Icon = $block['data']['button_button_1_icon'] ?? '';
+    $button1Icon = $block['data']['button_button_1_icon'] ?? '';
+    if (!empty($button1Icon)) {
+        $iconData = json_decode($button1Icon, true);
+        if (isset($iconData['id'], $iconData['style'])) {
+            $button1Icon = 'fa-' . $iconData['style'] . ' fa-' . $iconData['id'];
+        }
+    }
     $button2Text = $block['data']['button_button_2']['title'] ?? '';
     $button2Link = $block['data']['button_button_2']['url'] ?? '';
     $button2Target = $block['data']['button_button_2']['target'] ?? '_self';
     $button2Color = $block['data']['button_button_2_color'] ?? '';
     $button2Style = $block['data']['button_button_2_style'] ?? '';
+    $button2Download = $block['data']['button_button_2_download'] ?? false;
+    $button2Icon = $block['data']['button_button_2_icon'] ?? '';
+     if (!empty($button2Icon)) {
+        $iconData = json_decode($button2Icon, true);
+        if (isset($iconData['id'], $iconData['style'])) {
+            $button2Icon = 'fa-' . $iconData['style'] . ' fa-' . $iconData['id'];
+        }
+    }
+
 
     // Blokinstellingen
     $blockWidth = $block['data']['block_width'] ?? 100;
@@ -48,6 +66,7 @@
     $overlayEnabled = $block['data']['overlay_image'] ?? false;
     $overlayColor = $block['data']['overlay_color'] ?? '';
     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
+    $backgroundImageParallax = $block['data']['background_image_parallax'] ?? false;
 
     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
     $hideBlock = $block['data']['hide_block'] ?? false;
@@ -89,7 +108,7 @@
 @endphp
 
 <section id="cta-button" class="block-cta-button relative cta-button-{{ $randomNumber }}-custom-padding cta-button-{{ $randomNumber }}-custom-margin bg-{{ $backgroundColor }} {{ $customBlockClasses }} {{ $hideBlock ? 'hidden' : '' }}"
-         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
+         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; @if($backgroundImageParallax)	background-attachment: fixed; @endif background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
@@ -114,7 +133,7 @@
 
         <div class="cta-block relative z-10 mx-auto {{ $blockClass }} @if($blockWidth !== 'fullscreen') md:px-8 @endif">
 
-            <div class="background px-8 py-16 bg-{{ $blockBackgroundColor }} @if($blockWidth !== 'fullscreen') md:rounded-{{ $borderRadius }} @endif"
+            <div class="background relative px-8 py-16 bg-{{ $blockBackgroundColor }} @if($blockWidth !== 'fullscreen') md:rounded-{{ $borderRadius }} @endif"
                  style="background-image: url('{{ wp_get_attachment_image_url($blockBackgroundImage, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($blockBackgroundImage) }}">
                 @if ($blockOverlayEnabled)
                     <div class="absolute inset-0 bg-{{ $blockOverlayColor }} opacity-{{ $blockOverlayOpacity }} @if($blockWidth !== 'fullscreen') md:rounded-{{ $borderRadius }} @endif"></div>
@@ -124,7 +143,6 @@
 
                     <div class="cta-data flex flex-col @if($sideImage) order-2 xl:order-1 w-full xl:w-1/2 flex-col @endif md:{{ $flexDirection }} {{ $flexClass }} justify-center gap-x-16 gap-y-4 @if($topImage) mt-16 md:mt-20 @endif">
                         <div class="w-fit @if($ctaLayout == 'vertical' && $textPosition !== 'center') {{ $textClass }} @else text-center @endif md:{{ $textClass }}">
-
                             @if ($subTitle)
                                 <span class="subtitle block mb-2 text-{{ $titleColor }}">{!! $subTitle !!}</span>
                             @endif
@@ -139,24 +157,28 @@
                             @endif
                         </div>
                         @if (($button1Text) && ($button1Link))
-                            <div class="buttons flex  flex-wrap gap-x-4 w-fit justify-center md:justify-start">
+                            <div class="buttons flex flex-wrap gap-x-4 w-fit justify-center md:justify-start">
                                 @include('components.buttons.default', [
-                                   'text' => $button1Text,
-                                   'href' => $button1Link,
-                                   'alt' => $button1Text,
-                                   'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                                   'class' => 'rounded-lg text-left',
-                                   'target' => $button1Target,
-                               ])
+                                  'text' => $button1Text,
+                                  'href' => $button1Link,
+                                  'alt' => $button1Text,
+                                  'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
+                                  'class' => 'rounded-lg',
+                                  'target' => $button1Target,
+                                  'icon' => $button1Icon,
+                                  'download' => $button1Download,
+                                ])
                                 @if (($button2Text) && ($button2Link))
                                     @include('components.buttons.default', [
-                                       'text' => $button2Text,
-                                       'href' => $button2Link,
-                                       'alt' => $button2Text,
-                                       'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
-                                       'class' => 'rounded-lg text-left',
-                                       'target' => $button2Target,
-                                   ])
+                                        'text' => $button2Text,
+                                        'href' => $button2Link,
+                                        'alt' => $button2Text,
+                                        'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
+                                        'class' => 'rounded-lg',
+                                        'target' => $button2Target,
+                                        'icon' => $button2Icon,
+                                        'download' => $button2Download,
+                                    ])
                                 @endif
                             </div>
                         @endif
