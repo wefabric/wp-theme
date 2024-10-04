b4b82908b7f7ee6b11943638a6f2b7fa5b6a32ba
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-10-04 14:39:01 +0200

    Added new button options to result block

--- views/gutenberg/blocks/resultaten/index.blade.php
+++ views/gutenberg/blocks/resultaten/index.blade.php
@@ -6,21 +6,34 @@
     $text = $block['data']['text'] ?? '';
     $textColor = $block['data']['text_color'] ?? '';
 
-    $resultTitleColor = $block['data']['result_title_color'] ?? '';
-    $resultTextColor = $block['data']['result_text_color'] ?? '';
-    $resultBackgroundColor = $block['data']['result_background_color'] ?? '';
-
         // Buttons
         $button1Text = $block['data']['button_button_1']['title'] ?? '';
         $button1Link = $block['data']['button_button_1']['url'] ?? '';
         $button1Target = $block['data']['button_button_1']['target'] ?? '_self';
         $button1Color = $block['data']['button_button_1_color'] ?? '';
         $button1Style = $block['data']['button_button_1_style'] ?? '';
+        $button1Download = $block['data']['button_button_1_download'] ?? false;
+        $button1Icon = $block['data']['button_button_1_icon'] ?? '';
+        $button1Icon = $block['data']['button_button_1_icon'] ?? '';
+        if (!empty($button1Icon)) {
+            $iconData = json_decode($button1Icon, true);
+            if (isset($iconData['id'], $iconData['style'])) {
+                $button1Icon = 'fa-' . $iconData['style'] . ' fa-' . $iconData['id'];
+            }
+        }
         $button2Text = $block['data']['button_button_2']['title'] ?? '';
         $button2Link = $block['data']['button_button_2']['url'] ?? '';
         $button2Target = $block['data']['button_button_2']['target'] ?? '_self';
         $button2Color = $block['data']['button_button_2_color'] ?? '';
         $button2Style = $block['data']['button_button_2_style'] ?? '';
+        $button2Download = $block['data']['button_button_2_download'] ?? false;
+        $button2Icon = $block['data']['button_button_2_icon'] ?? '';
+        if (!empty($button2Icon)) {
+            $iconData = json_decode($button2Icon, true);
+            if (isset($iconData['id'], $iconData['style'])) {
+                $button2Icon = 'fa-' . $iconData['style'] . ' fa-' . $iconData['id'];
+            }
+        }
 
     $textPosition = $block['data']['text_position'] ?? '';
     $textOrder = $textPosition === 'left' ? 'lg:order-1 left' : 'lg:order-2 right';
@@ -48,6 +61,10 @@
 
 
     // Resultaten
+    $resultTitleColor = $block['data']['result_title_color'] ?? '';
+    $resultTextColor = $block['data']['result_text_color'] ?? '';
+    $resultBackgroundColor = $block['data']['result_background_color'] ?? '';
+
     $results = [];
     for ($i = 0; $i < 4; $i++) {
         $result_title_key = "results_{$i}_result_title";
@@ -73,6 +90,7 @@
     $overlayEnabled = $block['data']['overlay_image'] ?? false;
     $overlayColor = $block['data']['overlay_color'] ?? '';
     $overlayOpacity = $block['data']['overlay_opacity'] ?? '';
+    $backgroundImageParallax = $block['data']['background_image_parallax'] ?? false;
 
     $customBlockClasses = $block['data']['custom_css_classes'] ?? '';
     $hideBlock = $block['data']['hide_block'] ?? false;
@@ -117,9 +135,8 @@
     $numberAnimation = $block['data']['number_animation'] ?? false;
 @endphp
 
-<section id="resultaten"
-         class="block-resultaten relative resultaten-{{ $randomNumber }}-custom-padding resultaten-{{ $randomNumber }}-custom-margin bg-{{ $backgroundColor }} {{ $customBlockClasses }} {{ $hideBlock ? 'hidden' : '' }}"
-         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
+<section id="resultaten" class="block-resultaten relative resultaten-{{ $randomNumber }}-custom-padding resultaten-{{ $randomNumber }}-custom-margin bg-{{ $backgroundColor }} {{ $customBlockClasses }} {{ $hideBlock ? 'hidden' : '' }}"
+         style="background-image: url('{{ wp_get_attachment_image_url($backgroundImageId, 'full') }}'); background-repeat: no-repeat; @if($backgroundImageParallax)	background-attachment: fixed; @endif background-size: cover; {{ \Theme\Helpers\FocalPoint::getBackgroundPosition($backgroundImageId) }}">
     @if ($overlayEnabled)
         <div class="overlay absolute inset-0 bg-{{ $overlayColor }} opacity-{{ $overlayOpacity }}"></div>
     @endif
@@ -140,24 +157,28 @@
                         ])
                     @endif
                     @if (($button1Text) && ($button1Link))
-                        <div class="flex gap-4 mt-4 md:mt-8">
+                        <div class="buttons w-full flex flex-wrap gap-x-4 gap-y-2 mt-4 md:mt-8">
                             @include('components.buttons.default', [
                                'text' => $button1Text,
                                'href' => $button1Link,
                                'alt' => $button1Text,
                                'colors' => 'btn-' . $button1Color . ' btn-' . $button1Style,
-                               'class' => 'rounded-lg text-left',
+                               'class' => 'rounded-lg',
                                'target' => $button1Target,
-                           ])
+                               'icon' => $button1Icon,
+                               'download' => $button1Download,
+                            ])
                             @if (($button2Text) && ($button2Link))
                                 @include('components.buttons.default', [
-                                   'text' => $button2Text,
-                                   'href' => $button2Link,
-                                   'alt' => $button2Text,
-                                   'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
-                                   'class' => 'rounded-lg text-left',
-                                   'target' => $button2Target,
-                               ])
+                                    'text' => $button2Text,
+                                    'href' => $button2Link,
+                                    'alt' => $button2Text,
+                                    'colors' => 'btn-' . $button2Color . ' btn-' . $button2Style,
+                                    'class' => 'rounded-lg',
+                                    'target' => $button2Target,
+                                    'icon' => $button2Icon,
+                                    'download' => $button2Download,
+                                ])
                             @endif
                         </div>
                     @endif
