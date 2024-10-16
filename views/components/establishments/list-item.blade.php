53226d736ffbc16d6b7cfd04cdea1f9aac377456
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-10-16 10:24:07 +0200

    Update list-item.blade.php

--- views/components/establishments/list-item.blade.php
+++ views/components/establishments/list-item.blade.php
@@ -19,8 +19,7 @@
 
 {{-- todo: add check if link for hover effect--}}
     <div class="establishment-card h-full flex flex-col items-center @if ($link) group-hover:-translate-y-4 duration-300 ease-in-out @endif">
-
-        <div class="custom-height max-h-[360px] overflow-hidden w-full rounded-{{ $borderRadius }}">
+        <div class="image-container custom-height max-h-[360px] overflow-hidden w-full relative rounded-{{ $borderRadius }}">
             @include('components.image', [
                  'image_id' => $establishmentImage,
                  'size' => 'job-thumbnail',
@@ -30,19 +29,21 @@
             ])
         </div>
 
-        <div class="establishment-info w-full mt-5 flex flex-col">
+        <div class="establishment-info flex flex-col w-full grow mt-5">
             @if (!empty($visibleElements) && in_array('name', $visibleElements))
-                <p class="establishment-text font-bold text-lg text-{{ $establishmentTitleColor }}">{!! $establishmentName !!}</p>
+                <p class="establishment-name font-bold text-lg text-{{ $establishmentTitleColor }}">{!! $establishmentName !!}</p>
             @endif
 
-            @if (!empty($visibleElements) && in_array('address', $visibleElements))
-                <p class="establishment-address flex items-baseline leading-[1.5] text-{{ $establishmentTextColor }}">
-                    <i class="w-4 object-cover fas fa-map-marker-alt mr-3"></i>
-                    {!! $establishmentStreet . ' ' . $establishmentHouseNumber . $establishmentHouseNumberAddition !!}
-                    <br>
-                    {!! $establishmentZipCode . ' ' . $establishmentCity !!}
-                </p>
-            @endif
+            <div class="establishment-data mt-4 text-{{ $establishmentTextColor }}">
+                @if (!empty($visibleElements) && in_array('address', $visibleElements))
+                    <p class="establishment-address flex items-baseline leading-[1.5]">
+                        <i class="w-4 object-cover fas fa-map-marker-alt mr-3"></i>
+                        {!! $establishmentStreet . ' ' . $establishmentHouseNumber . $establishmentHouseNumberAddition !!}
+                        <br>
+                        {!! $establishmentZipCode . ' ' . $establishmentCity !!}
+                    </p>
+                @endif
+            </div>
 
             {{-- todo: add contact information --}}
 
