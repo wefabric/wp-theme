e6caed4433749ff387da2a05ab5e4795690e2216
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-10-29 11:16:06 +0100

    Updated country block

--- views/gutenberg/blocks/landen/index.blade.php
+++ views/gutenberg/blocks/landen/index.blade.php
@@ -93,7 +93,7 @@
             @if ($title)
                 <h2 class="text-{{ $titleColor }} container mx-auto lg:mb-4 @if($blockWidth == 'fullscreen') px-8 @endif {{ $titleClass }}">{!! $title !!}</h2>
             @endif
-            @if($countries)
+            @if ($countries)
                 @include('components.countries.list', ['countries' => $countries])
             @endif
             @if (($button1Text) && ($button1Link))
