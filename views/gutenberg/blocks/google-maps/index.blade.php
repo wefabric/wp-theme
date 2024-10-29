5fa6b6e16ce40fc25a3e4afcdae03d98ee531b15
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-10-29 16:17:38 +0100

    Updated Google maps block

--- views/gutenberg/blocks/google-maps/index.blade.php
+++ views/gutenberg/blocks/google-maps/index.blade.php
@@ -118,7 +118,7 @@
                 ])
             @endif
             <iframe width="100%" height="600"
-                    class="h-[300px] lg:h-[600px]"
+                    class="h-[300px] md:h-[400px] xl:h-[600px]"
                     frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
                     src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q={{ $mapsCity }} {{ $mapsStreet }} {{$mapsHouseNumber}}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
             </iframe>
