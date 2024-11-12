6064cf568092c2236064a12552c062015bed71a0
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-11-12 12:21:48 +0100

    Added class for breadcrumb location

--- views/components/breadcrumbs/index.blade.php
+++ views/components/breadcrumbs/index.blade.php
@@ -1,4 +1,4 @@
-<div class="breadcrumbs {{ $headerName }} w-full py-4 lg:py-8 bg-{{ $breadcrumbsBackgroundColor ?? 'bg-transparent' }} text-{{ $breadcrumbsTextColor ?? 'text-black' }}">
+<div class="breadcrumbs breadcrumbs-{{ $breadcrumbsLocation }} {{ $headerName }} w-full py-4 lg:py-8 bg-{{ $breadcrumbsBackgroundColor ?? 'bg-transparent' }} text-{{ $breadcrumbsTextColor ?? 'text-black' }}">
     <div class="relative z-10 @if ($breadcrumbsLocation === 'underneath' || $breadcrumbsLocation === 'above') px-8 container mx-auto @endif">
         <div>
             @php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); @endphp
