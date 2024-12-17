77a7b2d087bfb7d6cbdc16e95ffe70e9be2c4bd3
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2024-12-17 19:30:32 +0100

    Updated search result page

--- views/components/post-content-search.blade.php
+++ views/components/post-content-search.blade.php
@@ -1,17 +1,23 @@
-<article id="post-{{ Loop::id() }}" class="grid grid-cols-2 gap-x-16 mt-8" {!! post_class() !!}>
-    <div class="w-full h-auto">
-        <a href="{{ esc_url(get_permalink()) }}" rel="bookmark">
-            {!! the_post_thumbnail('small') !!}
+<article id="post-{{ Loop::id() }}" class="group grid grid-cols-2 gap-x-16 mt-8" {!! post_class() !!}>
+    <div class="w-full h-auto h-[300px]" style="height: 300px;">
+        <a href="{{ esc_url(get_permalink()) }}" rel="bookmark" class="h-full w-full">
+            @if(has_post_thumbnail())
+                {!! the_post_thumbnail('full', [
+                    'style' => 'height: 100%; width: 100%; object-fit: cover;'
+                ]) !!}
+                @else
+                <div class="h-full w-full bg-primary"></div>
+            @endif
         </a>
     </div>
     <div>
         <h2 class="entry-title">
-            <a href="{{ esc_url(get_permalink()) }}" rel="bookmark">{!! Loop::title() !!}</a>
+            <a class="group-hover:text-primary" href="{{ esc_url(get_permalink()) }}" rel="bookmark">{!! Loop::title() !!}</a>
         </h2>
         @if('post' === get_post_type())
             <div class="entry-meta">
-                {!! posted_on() !!}
-                {!! posted_by() !!}
+{{--                {!! posted_on() !!}--}}
+{{--                {!! posted_by() !!}--}}
             </div>
         @endif
         <div class="entry-summary">
