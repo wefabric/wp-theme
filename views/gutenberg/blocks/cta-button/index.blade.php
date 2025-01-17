84f9e37f87031824fea2ded7a1a12afd992b231f
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-01-17 16:38:15 +0100

    Update index.blade.php

--- views/gutenberg/blocks/cta-button/index.blade.php
+++ views/gutenberg/blocks/cta-button/index.blade.php
@@ -142,7 +142,7 @@
                 <div class="cta-wrapper container mx-auto @if($blockWidth == 'fullscreen') px-8 @else w-full md:px-16 @endif relative z-10 @if($sideImage) flex flex-col xl:flex-row justify-between items-center gap-8 !px-0 xl:!px-16 @endif">
 
                     <div class="cta-data flex flex-col @if($sideImage) order-2 xl:order-1 w-full xl:w-1/2 flex-col @endif md:{{ $flexDirection }} {{ $flexClass }} justify-center gap-x-16 gap-y-4 @if($topImage) mt-16 md:mt-20 @endif">
-                        <div class="w-fit @if($ctaLayout == 'vertical' && $textPosition !== 'center') {{ $textClass }} @else text-center @endif md:{{ $textClass }}">
+                        <div class="cta-data-styling w-fit @if($ctaLayout == 'vertical' && $textPosition !== 'center') {{ $textClass }} @else text-center @endif md:{{ $textClass }}">
                             @if ($subTitle)
                                 <span class="subtitle block mb-2 text-{{ $subTitleColor }}">{!! $subTitle !!}</span>
                             @endif
