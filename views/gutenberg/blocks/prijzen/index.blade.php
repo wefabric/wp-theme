5257ba98832f5b06cce4eb81f2e92e9d63bce972
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-02-17 09:52:31 +0100

    Updated prijs fields

--- views/gutenberg/blocks/prijzen/index.blade.php
+++ views/gutenberg/blocks/prijzen/index.blade.php
@@ -63,7 +63,7 @@
 
     // Theme settings
     $options = get_fields('option');
-    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength']??'': 'rounded-none';
+    $borderRadius = $options['rounded_design'] === true ? $options['border_radius_strength'] ?? '' : 'rounded-none';
 @endphp
 
 <section id="prijs-pakketten" class="block-prijs-pakketten relative bg-{{ $backgroundColor }} {{ $customBlockClasses }}"
