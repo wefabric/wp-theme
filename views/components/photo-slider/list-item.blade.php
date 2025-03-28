696b01942d31d5b3edc6ab65538deecc2978491e
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-03-28 11:20:39 +0100

    Updated photo slider image-item

--- views/components/photo-slider/list-item.blade.php
+++ views/components/photo-slider/list-item.blade.php
@@ -9,20 +9,22 @@
 @endphp
 
 <div class="image-item group h-full w-full @if ($flyinEffect) image-hidden @endif">
-    @if ($imageId)
-        @if ($imageLinkUrl)
-            <div class="card-overlay absolute w-full h-full overflow-hidden rounded-{{ $borderRadius }}">
-                <a href="{{ $imageLinkUrl }}" target="{{ $imageLinkTarget }}" aria-label="Ga naar {{ $imageLinkTitle }} pagina" class="absolute left-0 top-0 w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out"></a>
-            </div>
+    <div class="relative">
+        @if ($imageId)
+            @if ($imageLinkUrl)
+                <div class="card-overlay absolute w-full h-full overflow-hidden rounded-{{ $borderRadius }}">
+                    <a href="{{ $imageLinkUrl }}" target="{{ $imageLinkTarget }}" aria-label="Ga naar {{ $imageLinkTitle }} pagina" class="absolute left-0 top-0 w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out"></a>
+                </div>
+            @endif
+            @include('components.image', [
+                'image_id' => $imageId,
+                'size' => 'full',
+                'object_fit' => $imageStyle,
+                'img_class' => 'w-full h-full rounded-' . $borderRadius,
+                'alt' => $imageAlt
+            ])
         @endif
-        @include('components.image', [
-            'image_id' => $imageId,
-            'size' => 'full',
-            'object_fit' => $imageStyle,
-            'img_class' => 'w-full h-full rounded-' . $borderRadius,
-            'alt' => $imageAlt
-        ])
-    @endif
+    </div>
     @if ($imageCaption)
         @if ($imageLinkUrl) <a href="{{ $imageLinkUrl }}" target="{{ $imageLinkTarget }}" aria-label="Ga naar {{ $imageLinkTitle }}">@endif
             <div class="caption-text text-lg text-left font-bold mt-2 text-{{ $captionColor }}">{!! $imageCaption !!}</div>
