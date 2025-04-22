58fab038bbb06675dbb268c8decea42009a709c1
Leo Flapper <dominicus1991@gmail.com>
2025-04-22 22:15:39 +0200

    Footer cleanup

--- views/components/footer/contact.blade.php
+++ views/components/footer/contact.blade.php
@@ -117,34 +117,13 @@
 						<span class="inline-block pt-1">Route</span>
 						@include('components.link.closing')
 					@endif
-
 				</div>
 
-				<div class="opening-hours-section">
-					{{-- Opening hours --}}
-					@if(in_array('establishment_opening_hours', $establishmentElements) && !empty($opening_hours))
-						<div class="opening-hours-section">
-							<div class="opening-hours-text font-bold mb-2">Openingstijden</div>
-							<div class="flex flex-col">
-								@foreach ($opening_hours as $day)
-									<div class="flex items-center sm:gap-x-4 justify-between sm:justify-start">
-										<span class="w-fit sm:w-[100px]">{{ $day['day'] ?? 'Onbekend' }}</span>
-										@if (!empty($day['closed']) && $day['closed'])
-											<span>Gesloten</span>
-										@else
-											<span>
-												{{ $day['opening_hour'] ?? '00:00' }} uur - {{ $day['closing_hour'] ?? '00:00' }} uur
-												@if (!empty($day['opening_hour_2']) && !empty($day['closing_hour_2']))
-													& {{ $day['opening_hour_2'] }} uur - {{ $day['closing_hour_2'] }} uur
-												@endif
-											</span>
-										@endif
-									</div>
-								@endforeach
-							</div>
-						</div>
-					@endif
-				</div>
+				@include('components.footer.establishment-opening-hours', [
+					'establishmentElements' => $establishmentElements,
+					'establishment' => $establishment,
+					'opening_hours' => $opening_hours
+				])
 
 				{{--			Dit kan later weer aan--}}
 {{--				@include('components.establishments.directions')--}}
