317cf31ce13fd1aa541021e5374743ad7549d1fc
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-04-22 12:59:53 +0200

    Update contact.blade.php

--- views/components/footer/contact.blade.php
+++ views/components/footer/contact.blade.php
@@ -127,15 +127,15 @@
 							<div class="opening-hours-text font-bold mb-2">Openingstijden</div>
 							<div class="flex flex-col">
 								@foreach ($opening_hours as $day)
-									<div class="flex items-center sm:gap-x-12 justify-between sm:justify-start">
-										<span class="w-fit sm:w-[120px]">{{ $day['day'] ?? 'Onbekend' }}</span>
+									<div class="flex items-center sm:gap-x-6 justify-between sm:justify-start">
+										<span class="w-fit sm:w-[100px]">{{ $day['day'] ?? 'Onbekend' }}</span>
 										@if (!empty($day['closed']) && $day['closed'])
 											<span>Gesloten</span>
 										@else
 											<span>
-												{{ $day['opening_hour'] ?? '00:00' }} - {{ $day['closing_hour'] ?? '00:00' }}
+												{{ $day['opening_hour'] ?? '00:00' }} uur - {{ $day['closing_hour'] ?? '00:00' }} uur
 												@if (!empty($day['opening_hour_2']) && !empty($day['closing_hour_2']))
-													& {{ $day['opening_hour_2'] }} - {{ $day['closing_hour_2'] }}
+													& {{ $day['opening_hour_2'] }} uur - {{ $day['closing_hour_2'] }} uur
 												@endif
 											</span>
 										@endif
