dbc00abf1f5e438abc503f0ce4d39baf8aa00404
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-05-01 12:40:16 +0200

    Added image option to program block

--- views/components/programs/list-item.blade.php
+++ views/components/programs/list-item.blade.php
@@ -4,6 +4,10 @@
     $programTitle = get_the_title($program) ?? '';
 
     $visibleElements = $block['data']['show_element'] ?? [];
+
+    $hasImage = collect($programItems)->contains(function ($item) {
+        return !empty($item['image']);
+    });
 @endphp
 
 <div class="flex flex-col gap-y-4">
@@ -14,6 +18,19 @@
 
     @foreach ($programItems as $programItem)
         <div class="program-item flex flex-col md:flex-row gap-y-2 gap-x-4 md:gap-x-8 w-full border-b pb-4">
+            @if ($hasImage)
+                <div class="image md:w-32 shrink-0">
+                    @if (!empty($programItem['image']))
+                        @include('components.image', [
+                           'image_id' => $programItem['image'],
+                           'size' => 'full',
+                           'object_fit' => 'cover',
+                           'img_class' => 'w-full h-auto object-cover',
+                           'alt' => $programItem['title']
+                        ])
+                    @endif
+                </div>
+            @endif
             <div class="time md:w-32 shrink-0">
                 <div class="program-item-time font-bold text-{{ $programTitleColor }}">{!! $programItem['time'] !!}</div>
             </div>
@@ -25,4 +42,5 @@
             </div>
         </div>
     @endforeach
+
 </div>
