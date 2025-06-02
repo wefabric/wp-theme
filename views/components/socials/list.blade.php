c1708352b39a64405f622f972af625d7973608df
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-06-02 14:26:15 +0200

    Update list.blade.php

--- views/components/socials/list.blade.php
+++ views/components/socials/list.blade.php
@@ -6,20 +6,13 @@
 @endphp
 
 @if(!empty($option) && array_key_exists('channels', $option))
-    <div class="socials mb-4 flex gap-4 items-center">
-        {{--        @foreach($option['channels'] as $social)--}}
-        {{--            @include('components.buttons.icon', [--}}
-        {{--	            'href' => $social['url'],--}}
-        {{--	            'alt' => $social['name'],--}}
-        {{--	            'rel' => 'noopener',--}}
-        {{--	            'target' => '_blank',--}}
-        {{--	            'icon' => $social['icon'] .' text-xl',--}}
-        {{--	            'a_class' => 'no-underline '--}}
-        {{--            ])--}}
-        {{--        @endforeach--}}
+    <div class="socials mb-4 flex gap-3 items-center flex-wrap">
         @foreach($option['channels'] as $social)
-            <a class="group footer-social social-{{ strtolower($social['name']) }} hover:text-{{ $title_color }}" href="{{ $social['url'] }}" alt="{{ $social['name'] }}" target="_blank" aria-label="Ga naar {{ $social['name'] }}"><i
-                        class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110"></i></a>
+            <a class="group footer-social social-{{ strtolower($social['name']) }} hover:text-{{ $title_color }}"
+               href="{{ $social['url'] }}" alt="{{ $social['name'] }}" target="_blank"
+               aria-label="Ga naar {{ $social['name'] }}">
+                <i class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110"></i>
+            </a>
         @endforeach
     </div>
 @endif
