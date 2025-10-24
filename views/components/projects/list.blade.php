652adf1bd86a8e050d0f959d8a89bfa8042c53b2
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-10-24 13:32:05 +0200

    Fix for project navigation

--- views/components/projects/list.blade.php
+++ views/components/projects/list.blade.php
@@ -17,7 +17,10 @@
     $swiperLoop = $block['data']['loop_slides'] ?? true;
     $swiperCenteredSlides = $block['data']['centered_slides'] ?? false;
     $randomNumber = rand(0, 1000);
+    $paginationStyle = $block['data']['pagination_style'] ?? 'bullets';
     $randomId = 'projectSwiper-' . $randomNumber;
+
+    $spaceBetween = $block['data']['space_between'] ?? 20;
 @endphp
 
 @if($block['data']['show_slider'])
@@ -30,7 +33,9 @@
                     </div>
                 @endforeach
             </div>
-            <div class="swiper-pagination"></div>
+            @if ($paginationStyle != 'none')
+                <div class="swiper-pagination"></div>
+            @endif
         </div>
         <div class="swiper-navigation">
             <div class="swiper-button-next project-button-next-{{ $randomNumber }}"></div>
@@ -56,7 +61,7 @@
 <script>
     window.addEventListener("DOMContentLoaded", (event) => {
         var projectSwiper = new Swiper(".{{ $randomId }}", {
-            spaceBetween: 20,
+            spaceBetween: {{ $spaceBetween }},
             @if ($swiperCenteredSlides)
                 centeredSlides: true,
             @endif
@@ -66,10 +71,16 @@
                     disableOnInteraction: true,
                 },
             @endif
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
+            @if ($paginationStyle != 'none')
+                pagination: {
+                    el: '.swiper-pagination',
+                    @if ($paginationStyle == 'progress_bar')
+                        type: 'progressbar',
+                    @elseif ($paginationStyle == 'bullets')
+                        clickable: true,
+                    @endif
+                },
+            @endif
             navigation: {
                 nextEl: ".project-button-next-{{ $randomNumber }}",
                 prevEl: ".project-button-prev-{{ $randomNumber }}",
