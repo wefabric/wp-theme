d5a768a52c7061bd61ebfc9202003a612e0abbad
basberswerdawefabric <bas@wefabric.nl>
2025-11-05 14:39:32 +0100

    Fix for rendering shop content

--- views/shop/single.blade.php
+++ views/shop/single.blade.php
@@ -2,37 +2,37 @@
 
 @section('content')
 
-	@include('components.breadcrumbs.index')
+    @include('components.breadcrumbs.index')
 
-	<div class="container mx-auto">
-		@php(do_action('woocommerce_before_main_content'))
+    <div class="container mx-auto">
+        @php(do_action('woocommerce_before_main_content'))
 
-		<div class="woocommerce-product-container">
-			@loop
+        <div class="woocommerce-product-container">
+            @loop
 
-{{--			@php(do_action('woocommerce_before_single_product'))--}}
+            {{--			@php(do_action('woocommerce_before_single_product'))--}}
 
-			<div id="product-{{ Loop::id() }}" {{ wc_product_class('lg:mt-24 lg:grid-cols-10 lg:gap-16 px-8') }}>
-				<div class="before-summary order-1 lg:order-1 lg:col-span-5">
-					@php(do_action('woocommerce_before_single_product_summary'))
-				</div>
+            <div id="product-{{ Loop::id() }}" {{ wc_product_class('lg:mt-24 lg:grid-cols-10 lg:gap-16 px-8') }}>
+                <div class="before-summary order-1 lg:order-1 lg:col-span-5">
+                    @php(do_action('woocommerce_before_single_product_summary'))
+                </div>
 
-				<div class="summary entry-summary order-1 lg:order-2 lg:col-span-5">
-					@php(do_action('woocommerce_single_product_summary'))
-				</div>
+                <div class="summary entry-summary order-1 lg:order-2 lg:col-span-5">
+                    @php(do_action('woocommerce_single_product_summary'))
+                </div>
 
-				<div class="order-3 lg:col-span-10">
-					@php(do_action('woocommerce_after_single_product_summary'))
-				</div>
-			</div>
-			@php(do_action('woocommerce_after_single_product'))
-			@endloop
+                <div class="order-3 lg:col-span-10">
+                    @php(do_action('woocommerce_after_single_product_summary'))
+                </div>
+            </div>
+            @php(do_action('woocommerce_after_single_product'))
+            @endloop
 
-			@php(do_action('woocommerce_after_main_content'))
-		</div>
+            @php(do_action('woocommerce_after_main_content'))
+        </div>
 
-		<div class="page-builder">
-			{!! pageBuilder()->render(get_the_ID()) !!}
-		</div>
-	</div>
+        <div class="page-builder">
+            {!! the_content() !!}
+        </div>
+    </div>
 @endsection()
