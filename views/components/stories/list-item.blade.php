3520bef91659c30e649f358aafe7f8887508b775
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2025-12-02 14:32:50 +0100

    Updated story item

--- views/components/stories/list-item.blade.php
+++ views/components/stories/list-item.blade.php
@@ -16,22 +16,7 @@
             <div class="image-container max-h-[360px] overflow-hidden w-full relative rounded-{{ $borderRadius }}">
                 <a href="{{ $storyUrl }}" aria-label="Ga naar {{ $storyTitle }} pagina"
                    class="absolute w-full h-full bg-primary z-10 opacity-0 group-hover:opacity-50 transition-opacity duration-300 ease-in-out"></a>
-                @if (!empty($visibleElements) && in_array('category', $visibleElements))
-                    @if ($storyCategories && !is_bool($storyCategories))
-                        <div class="story-categories absolute z-20 top-[15px] left-[15px] flex flex-wrap gap-2">
-                            @foreach ($storyCategories as $category)
-                                @php
-                                    $categoryColor = get_field('category_color', $category);
-                                    $categoryIcon = get_field('category_icon', $category);
-                                @endphp
-                                <div style="background-color: {{ $categoryColor }}" class="story-category @if(empty($categoryColor)) bg-primary @endif text-white px-4 py-2 rounded-full flex items-center gap-x-1">
-                                    {!! $categoryIcon !!} {!! $category->name !!}
-                                </div>
-                            @endforeach
-                        </div>
 
-                    @endif
-                @endif
                 @include('components.image', [
                    'image_id' => $storyThumbnailID,
                    'size' => 'job-thumbnail',
@@ -39,31 +24,55 @@
                    'img_class' => 'aspect-square w-full h-full object-cover object-center transform ease-in-out duration-300 group-hover:scale-110',
                    'alt' => $storyTitle,
                 ])
+
+                <div class="absolute bottom-0 left-0 p-8 z-20">
+                    @if (!empty($visibleElements) && in_array('category', $visibleElements))
+                        @if ($storyCategories && !is_bool($storyCategories))
+                            <div class="story-categories z-20 flex flex-wrap gap-2">
+                                @foreach ($storyCategories as $category)
+                                    @php
+                                        $categoryColor = get_field('category_color', $category);
+                                        $categoryIcon = get_field('category_icon', $category);
+                                    @endphp
+                                    <div style="background-color: {{ $categoryColor }}" class="story-category @if(empty($categoryColor)) bg-primary @endif text-white px-4 py-2 rounded-full flex items-center gap-x-1">
+                                        {!! $categoryIcon !!} {!! $category->name !!}
+                                    </div>
+                                @endforeach
+                            </div>
+
+                        @endif
+                    @endif
+                    <a href="{{ $storyUrl }}" aria-label="Ga naar {{ $storyTitle }} pagina" class="story-title-text font-bold text-{{ $storyTitleColor }} text-lg group-hover:text-primary">{!! $storyTitle !!}</a>
+                </div>
+
             </div>
         @endif
-        <div class="story-content flex flex-col w-full grow mt-5">
 
-            <a href="{{ $storyUrl }}" aria-label="Ga naar {{ $storyTitle }} pagina" class="story-title-text font-bold text-{{ $storyTitleColor }} text-lg group-hover:text-primary">{!! $storyTitle !!}</a>
 
-            <div class="story-data mt-4 text-{{ $storyTextColor }}">
-                @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($storySummary))
-                    <p class="mt-3 mb-3">{{ $storySummary }}</p>
-                @endif
-            </div>
 
-            @if (!empty($visibleElements) && in_array('button', $visibleElements))
-                @if ($buttonCardText)
-                    <div class="mt-auto pt-8 z-10">
-                        @include('components.buttons.default', [
-                           'text' => $buttonCardText,
-                           'href' => $storyUrl,
-                           'alt' => $buttonCardText,
-                           'colors' => 'btn-' . $buttonCardColor . ' btn-' . $buttonCardStyle,
-                           'class' => 'rounded-lg',
-                       ])
-                    </div>
-                @endif
-            @endif
-        </div>
+
+{{--            <div class="story-content flex flex-col w-full grow mt-5">--}}
+
+
+{{--            <div class="story-data mt-4 text-{{ $storyTextColor }}">--}}
+{{--                @if (!empty($visibleElements) && in_array('overview_text', $visibleElements) && !empty($storySummary))--}}
+{{--                    <p class="mt-3 mb-3">{{ $storySummary }}</p>--}}
+{{--                @endif--}}
+{{--            </div>--}}
+
+{{--            @if (!empty($visibleElements) && in_array('button', $visibleElements))--}}
+{{--                @if ($buttonCardText)--}}
+{{--                    <div class="mt-auto pt-8 z-10">--}}
+{{--                        @include('components.buttons.default', [--}}
+{{--                           'text' => $buttonCardText,--}}
+{{--                           'href' => $storyUrl,--}}
+{{--                           'alt' => $buttonCardText,--}}
+{{--                           'colors' => 'btn-' . $buttonCardColor . ' btn-' . $buttonCardStyle,--}}
+{{--                           'class' => 'rounded-lg',--}}
+{{--                       ])--}}
+{{--                    </div>--}}
+{{--                @endif--}}
+{{--            @endif--}}
+{{--        </div>--}}
     </div>
 </div>
