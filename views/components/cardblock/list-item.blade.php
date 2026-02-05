b3b08355cecfcd4e71580206d5767e5c5efe0a0d
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2026-02-05 15:25:17 +0100

    Updated card block options

--- views/components/cardblock/list-item.blade.php
+++ views/components/cardblock/list-item.blade.php
@@ -10,13 +10,21 @@
     $pageId = $page['id'];
     $postType = get_post_type($pageId);
     $terms = [];
-    if ($postType === 'page') {
-            $terms = get_the_category($pageId) ?? [];
-        } elseif ($postType === 'post') {
-            $terms = get_the_category($pageId) ?? [];
-        } else {
-        // Fetch terms from the custom taxonomy 'CPT_categories'
-        $terms = get_the_terms($pageId, $postType . '_categories') ?? [];
+    if ($postType === 'page' || $postType === 'post') {
+        $terms = get_the_category($pageId) ?: [];
+    } else {
+        $taxonomies = get_object_taxonomies($postType);
+        if (!empty($taxonomies)) {
+            foreach ($taxonomies as $taxonomy) {
+                $tax_obj = get_taxonomy($taxonomy);
+                if (!$tax_obj || !$tax_obj->public || $taxonomy === 'post_format') continue;
+
+                $post_terms = get_the_terms($pageId, $taxonomy);
+                if ($post_terms && !is_wp_error($post_terms)) {
+                    $terms = array_merge($terms, (array)$post_terms);
+                }
+            }
+        }
     }
 
     // Weergave
