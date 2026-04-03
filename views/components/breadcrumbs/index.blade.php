be832dd6763ed5a8472f6fcc1682705841ffa6e5
Sebastiaan Hartman <sebastiaan@wefabric.nl>
2026-04-03 13:27:13 +0200

    Updated breadcrumbs styling class

--- views/components/breadcrumbs/index.blade.php
+++ views/components/breadcrumbs/index.blade.php
@@ -1,6 +1,6 @@
 <div class="breadcrumbs breadcrumbs-{{ $breadcrumbsLocation }} {{ $headerName }} w-full py-4 lg:py-8 bg-{{ $breadcrumbsBackgroundColor ?? 'bg-transparent' }} text-{{ $breadcrumbsTextColor ?? 'text-black' }}">
     <div class="relative z-10 @if ($breadcrumbsLocation !== 'inside' ) px-8 container mx-auto @endif">
-        <div>
+        <div class="breadcrumbs-wrapper">
             @php
                 if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs();
                 if (function_exists('yoast_breadcrumb')) echo yoast_breadcrumb();
