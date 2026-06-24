e42093754be20bdfcd263976f5839d0e56572a73
Leo Flapper <dominicus1991@gmail.com>
2026-06-24 07:32:19 +0200

    Externe links: URL velden omzetten naar link type met rel noopener
    - ACF settings-social-media.json: url subveld channels gewijzigd naar link type
    - ACF fields-logo.json: link veld gewijzigd naar link type
    - socials/list.blade.php: link array formaat ondersteund, rel noopener toegevoegd, ongeldig alt attribuut verwijderd
    - logos/list-item.blade.php: link array formaat ondersteund, rel noopener toegevoegd voor externe links

--- views/components/socials/list.blade.php
+++ views/components/socials/list.blade.php
@@ -8,11 +8,19 @@
 @if(!empty($option) && array_key_exists('channels', $option))
     <div class="socials mb-4 flex gap-3 items-center flex-wrap">
         @foreach($option['channels'] as $social)
-            <a class="group footer-social social-{{ strtolower($social['name']) }} hover:text-{{ $title_color }}"
-               href="{{ $social['url'] }}" title="{{ $social['name'] }} pagina" alt="{{ $social['name'] }}" target="_blank"
-               aria-label="Ga naar {{ $social['name'] }}">
-                <i class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110"></i>
-            </a>
+            @php
+                $socialLink = $social['url'];
+                $socialUrl = is_array($socialLink) ? ($socialLink['url'] ?? '') : $socialLink;
+                $socialTarget = is_array($socialLink) ? ($socialLink['target'] ?: '_blank') : '_blank';
+            @endphp
+            @if($socialUrl)
+                <a class="group footer-social social-{{ strtolower($social['name']) }} hover:text-{{ $title_color }}"
+                   href="{{ $socialUrl }}" title="{{ $social['name'] }} pagina" target="{{ $socialTarget }}"
+                   rel="noopener noreferrer"
+                   aria-label="Ga naar {{ $social['name'] }}">
+                    <i class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110"></i>
+                </a>
+            @endif
         @endforeach
     </div>
 @endif
