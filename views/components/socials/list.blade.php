2aacc48b684ea314184fb3ffbcb2e241a96a53ad
Leo Flapper <dominicus1991@gmail.com>
2026-07-22 22:21:32 +0200

    Toegankelijkheid: aria-hidden op iconen, aria-label op icon-only links
    - socials/list: aria-hidden="true" op <i> iconen
    - secondary-nav: aria-label op WhatsApp- en zoekknop-links, aria-hidden op alle <i> iconen
    - header-mobile: aria-label op icon-only telefoon/e-maillinks in hamburger en mobiel menu, aria-hidden op <i> iconen in alle secties

--- views/components/socials/list.blade.php
+++ views/components/socials/list.blade.php
@@ -11,7 +11,7 @@
             <a class="group footer-social social-{{ strtolower($social['name']) }} hover:text-{{ $title_color }}"
                href="{{ $social['url'] }}" title="{{ $social['name'] }} pagina" target="_blank" rel="noopener noreferrer"
                aria-label="Ga naar {{ $social['name'] }}">
-                <i class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110"></i>
+                <i class="{{ $social['icon'] }} text-xl hover:text-{{ $title_color }} transition-all group-hover:scale-110" aria-hidden="true"></i>
             </a>
         @endforeach
     </div>
